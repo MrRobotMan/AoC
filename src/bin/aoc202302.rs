e835289165cbf2691bcd58880b9c61c9b5399aec
David Weiss <dweiss2@gmail.com>
2023-12-01 23:56:59 -0600

    2023 Day 2 Part 1

--- src/bin/aoc202302.rs
+++ src/bin/aoc202302.rs
@@ -14,6 +14,7 @@ fn main() {
 #[derive(Default)]
 struct AocDay {
     games: HashMap<u32, Game>,
+    max: Game,
 }
 
 impl Runner for AocDay {
@@ -22,6 +23,11 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
+        self.max = Game {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
         for line in read_lines("inputs/2023/day02.txt") {
             if let Some((game, pulls)) = line.split_once(':') {
                 let mut red = 0;
@@ -53,12 +59,27 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(
+            self.games
+                .iter()
+                .filter_map(|(game_num, pulls)| {
+                    if valid_game(pulls, &self.max) {
+                        Some(game_num)
+                    } else {
+                        None
+                    }
+                })
+                .sum::<u32>(),
+        )
     }
 
     fn part2(&mut self) -> Vec<String> {
         output("Unsolved")
     }
+}
+
+fn valid_game(game: &Game, limit: &Game) -> bool {
+    game.red <= limit.red && game.green <= limit.green && game.blue <= limit.blue
 }
 
 #[derive(Debug, Default)]
@@ -69,12 +90,68 @@ struct Game {
 }
 #[cfg(test)]
 mod tests {
-    //use super::*;
+    use super::*;
 
     #[test]
-    fn test_game() {
-        let expected = 0;
-        let actual = 0;
+    fn test_part1() {
+        let expected = 8;
+        let games = HashMap::from([
+            (
+                1,
+                Game {
+                    red: 4,
+                    blue: 6,
+                    green: 2,
+                },
+            ),
+            (
+                2,
+                Game {
+                    red: 1,
+                    blue: 4,
+                    green: 3,
+                },
+            ),
+            (
+                3,
+                Game {
+                    red: 20,
+                    blue: 6,
+                    green: 13,
+                },
+            ),
+            (
+                4,
+                Game {
+                    red: 14,
+                    blue: 15,
+                    green: 3,
+                },
+            ),
+            (
+                5,
+                Game {
+                    red: 6,
+                    blue: 2,
+                    green: 3,
+                },
+            ),
+        ]);
+        let limit = Game {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+        let actual = games
+            .iter()
+            .filter_map(|(idx, game)| {
+                if valid_game(game, &limit) {
+                    Some(idx)
+                } else {
+                    None
+                }
+            })
+            .sum();
         assert_eq!(expected, actual);
     }
 }
