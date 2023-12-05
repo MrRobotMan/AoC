0d8b44a18ea13bde0c2f446393b76c073d0a8805
David Weiss <dweiss2@gmail.com>
2023-12-04 21:52:51 -0600

    2023 Day 4 Part 1

--- src/bin/aoc202304.rs
+++ src/bin/aoc202304.rs
@@ -26,7 +26,7 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(self.cards.iter().map(Card::score).sum::<i64>())
     }
 
     fn part2(&mut self) -> Vec<String> {
@@ -40,8 +40,20 @@ struct Card {
     plays: Vec<i64>,
 }
 
-impl From<&String> for Card {
-    fn from(value: &String) -> Self {
+impl Card {
+    fn score(&self) -> i64 {
+        let mut score = 0;
+        for play in &self.plays {
+            if self.winners.contains(play) {
+                score = 1.max(score * 2);
+            }
+        }
+        score
+    }
+}
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
         let (_, tail) = value.split_once(": ").unwrap();
         let (winners, plays) = tail.split_once(" | ").unwrap();
         Self {
@@ -58,7 +70,12 @@ impl From<&String> for Card {
 }
 impl From<String> for Card {
     fn from(value: String) -> Self {
-        (&value).into()
+        value.as_str().into()
+    }
+}
+impl From<&String> for Card {
+    fn from(value: &String) -> Self {
+        value.as_str().into()
     }
 }
 
@@ -75,6 +92,22 @@ mod tests {
         let actual = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
             .to_string()
             .into();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part1() {
+        let cards = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+            .split('\n')
+            .map(|l| l.into())
+            .collect::<Vec<Card>>();
+        let expected = 13;
+        let actual = cards.iter().map(Card::score).sum::<i64>();
         assert_eq!(expected, actual);
     }
 }
