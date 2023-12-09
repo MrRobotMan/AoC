a1beb07c6bc419120818667a37c86c2aeeb6c2da
David Weiss <dweiss2@gmail.com>
2023-12-09 01:10:47 -0600

    2023 Day 9 Part 1

--- src/bin/aoc202309.rs
+++ src/bin/aoc202309.rs
@@ -7,7 +7,7 @@ fn main() {
 
 #[derive(Default)]
 struct AocDay {
-    histories: Vec<Vec<i64>>,
+    histories: Vec<History>,
 }
 
 impl Runner for AocDay {
@@ -18,22 +18,96 @@ impl Runner for AocDay {
     fn parse(&mut self) {
         self.histories = aoc::read_lines("inputs/2023/day09.txt")
             .iter()
-            .map(|l| {
-                l.split_ascii_whitespace()
-                    .map(|n| n.parse().unwrap())
-                    .collect()
-            })
+            .map(|l| l.into())
             .collect();
-        for hist in &self.histories[..10] {
-            println!("{hist:?}");
-        }
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let mut last = self.histories.clone();
+        for hist in last.iter_mut() {
+            hist.build_next();
+        }
+        output(
+            last.iter()
+                .fold(0, |acc, hist| acc + hist.values.last().unwrap()),
+        )
     }
 
     fn part2(&mut self) -> Vec<String> {
         output("Unsolved")
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct History {
+    values: Vec<i64>,
+}
+
+impl History {
+    fn build_next(&mut self) {
+        let mut temp = self.values.clone();
+        let mut last = Vec::new();
+        while !zeroes(&temp) {
+            match temp.last() {
+                Some(v) => last.push(*v),
+                None => break,
+            }
+            temp = temp
+                .as_slice()
+                .windows(2)
+                .map(|vals| vals[1] - vals[0])
+                .collect();
+        }
+        last.reverse();
+        self.values
+            .push(last.into_iter().reduce(|acc, v| acc + v).unwrap());
+    }
+}
+fn zeroes(values: &[i64]) -> bool {
+    values.iter().all(|v| v == &0)
+}
+
+impl From<&String> for History {
+    fn from(value: &String) -> Self {
+        let values = value
+            .split_ascii_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+        Self { values }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: [[i64; 6]; 3] = [
+        [0, 3, 6, 9, 12, 15],
+        [1, 3, 6, 10, 15, 21],
+        [10, 13, 16, 21, 30, 45],
+    ];
+
+    #[test]
+    fn test_part1() {
+        let expected = 114;
+        let mut day = AocDay {
+            histories: INPUT
+                .iter()
+                .map(|l| History { values: l.to_vec() })
+                .collect(),
+        };
+        let actual = day.part1()[0].parse().unwrap_or(0);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_build_next() {
+        let expected = vec![1, 3, 6, 10, 15, 21, 28];
+        let mut history = History {
+            values: vec![1, 3, 6, 10, 15, 21],
+        };
+        history.build_next();
+        let actual = history.values;
+        assert_eq!(expected, actual);
     }
 }
