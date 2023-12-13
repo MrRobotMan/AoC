91abbe9ce80ca613de4e3eb8fed05eef51a5cc3d
David Weiss <dweiss2@gmail.com>
2023-12-13 10:02:47 -0600

    2023 Day 13 Part 1

--- src/bin/aoc202313.rs
+++ src/bin/aoc202313.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, fmt::Display};
+
 use aoc::runner::{output, run_solution, Runner};
 
 fn main() {
@@ -27,7 +29,7 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(self.patterns.iter().map(Pattern::score).sum::<usize>())
     }
 
     fn part2(&mut self) -> Vec<String> {
@@ -55,6 +57,76 @@ impl Pattern {
         }
         Self { rows, cols }
     }
+
+    fn score(&self) -> usize {
+        if let Some(row) = self.mirror_idx(Orientation::Rows) {
+            row * 100
+        } else if let Some(col) = self.mirror_idx(Orientation::Cols) {
+            col
+        } else {
+            panic!("No mirror line found for\n{self}");
+        }
+    }
+
+    fn mirror_idx(&self, orientation: Orientation) -> Option<usize> {
+        let arr = match orientation {
+            Orientation::Rows => &self.rows,
+            Orientation::Cols => &self.cols,
+        };
+        let mut candidates = Vec::new();
+        for (idx, loc) in arr.iter().enumerate() {
+            let mut options = HashSet::new();
+            for (line, other) in arr.iter().enumerate() {
+                if line == idx {
+                    continue;
+                }
+                if loc == other {
+                    options.insert((idx + line) / 2);
+                }
+            }
+            candidates.push(options);
+        }
+
+        let opts = candidates
+            .iter()
+            .fold(HashSet::new(), |mut acc: HashSet<usize>, o| {
+                acc.extend(o);
+                acc
+            });
+
+        for o in opts {
+            let range = (candidates.len() - o - 1).min(o + 1);
+            let left = o - (range - 1);
+            let right = o + range + 1;
+            let outside = (&candidates[..left], &candidates[right..]);
+            if !outside.0.is_empty() && !outside.1.is_empty() {
+                // Ony one outie can have sets.
+                continue;
+            }
+            if candidates[left..right].iter().all(|c| c.contains(&o)) {
+                return Some(o + 1);
+            }
+        }
+        None
+    }
+}
+
+impl Display for Pattern {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut lines = String::new();
+        for row in &self.rows {
+            for chr in row {
+                lines.push(*chr);
+            }
+            lines.push('\n');
+        }
+        write!(f, "{lines}")
+    }
+}
+
+enum Orientation {
+    Rows,
+    Cols,
 }
 
 #[cfg(test)]
@@ -105,5 +177,54 @@ mod tests {
 
         assert_eq!(expected.rows, actual.rows);
         assert_eq!(expected.cols, actual.cols);
+    }
+
+    #[test]
+    fn test_reflected_cols() {
+        let expected = 5;
+        let actual = Pattern::new(INPUT.split("\n\n").next().unwrap()).score();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_reflected_rows() {
+        let expected = 400;
+        let actual = Pattern::new(INPUT.split("\n\n").last().unwrap()).score();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_failing() {
+        let pattern = Pattern::new(
+            ".####..
+###..#.
+..#.###
+#.####.
+#.####.
+..#.###
+###..#.
+.####..
+.....#.
+...#.#.
+.####..
+###..#.
+..#.###",
+        );
+
+        let expected = 400;
+        let actual = pattern.score();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part1() {
+        let expected = 405;
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let actual = day.part1()[0].parse::<i32>().unwrap_or(0);
+        assert_eq!(expected, actual);
     }
 }
