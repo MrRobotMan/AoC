af277e318cb4cdc906bffee8fec7bfe57ce5ce20
David Weiss <dweiss2@gmail.com>
2023-12-14 07:00:39 -0600

    2023 Day 14 Template

---
+++ src/bin/aoc202314.rs
@@ -0,0 +1,30 @@
+use aoc::runner::{output, run_solution, Runner};
+
+fn main() {
+    let mut day = AocDay{input: "inputs/2023/day14.txt".into(), ..Default::default()};
+    run_solution(&mut day);
+}
+
+#[derive(Default)]
+struct AocDay {
+    input: String,
+}
+
+impl Runner for AocDay {
+    fn name(&self) -> (usize, usize) {
+        (2023, 14)
+    }
+
+    fn parse(&mut self) {
+        // Parse the input
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        output("Unsolved")
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        output("Unsolved")
+    }
+}
+        
