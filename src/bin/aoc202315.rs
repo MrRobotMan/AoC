fe3e30bf6f65121addc5bb452b78f6145e17fab0
David Weiss <dweiss2@gmail.com>
2023-12-15 08:22:33 -0600

    2023 Day 15 Parsing

--- src/bin/aoc202315.rs
+++ src/bin/aoc202315.rs
@@ -1,13 +1,17 @@
 use aoc::runner::{output, run_solution, Runner};
 
 fn main() {
-    let mut day = AocDay{input: "inputs/2023/day15.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/2023/day15.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    initialization: Vec<Vec<char>>,
 }
 
 impl Runner for AocDay {
@@ -16,7 +20,10 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        self.initialization = aoc::read_line(&self.input)
+            .split(|c| c == &',')
+            .map(|i| i.to_vec())
+            .collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -27,4 +34,34 @@ impl Runner for AocDay {
         output("Unsolved")
     }
 }
-        
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT: &str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+    #[test]
+    fn test_parse() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = vec![
+            vec!['r', 'n', '=', '1'],
+            vec!['c', 'm', '-'],
+            vec!['q', 'p', '=', '3'],
+            vec!['c', 'm', '=', '2'],
+            vec!['q', 'p', '-'],
+            vec!['p', 'c', '=', '4'],
+            vec!['o', 't', '=', '9'],
+            vec!['a', 'b', '=', '5'],
+            vec!['p', 'c', '-'],
+            vec!['p', 'c', '=', '6'],
+            vec!['o', 't', '=', '7'],
+        ];
+        let actual = day.initialization;
+        assert_eq!(expected, actual);
+    }
+}
