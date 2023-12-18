df49651e8aeca954324ae38a1286440a221cb6d9
David Weiss <dweiss2@gmail.com>
2023-12-18 12:54:23 -0600

    2023 Day 18 Part 2 Needs Boost.

--- src/bin/aoc202318.rs
+++ src/bin/aoc202318.rs
@@ -17,8 +17,6 @@ fn main() {
 struct AocDay {
     input: String,
     instructions: Vec<Instruction>,
-    border: HashMap<(i32, i32), char>,
-    dimensions: (i32, i32, i32, i32),
 }
 
 impl Runner for AocDay {
@@ -34,91 +32,108 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        self.build_map();
-        let mut vol = 0;
-        let mut inside = false;
-        let mut cur = '.';
-        for row in self.dimensions.0..=self.dimensions.1 {
-            for col in self.dimensions.2..=self.dimensions.3 + 1 {
-                match self.border.get(&(row, col)) {
-                    Some('|') => {
-                        vol += 1;
-                        inside = !inside;
-                    }
-                    Some('F') => {
-                        vol += 1;
-                        inside = !inside;
-                        cur = 'F';
-                    }
-                    Some('L') => {
-                        vol += 1;
-                        inside = !inside;
-                        cur = 'L';
-                    }
-                    Some('J') => {
-                        vol += 1;
-                        if cur == 'L' {
-                            inside = !inside;
-                            cur = '.';
-                        }
-                    }
-                    Some('7') => {
-                        vol += 1;
-                        if cur == 'F' {
-                            inside = !inside;
-                            cur = '.';
-                        }
-                    }
-                    Some('-') => vol += 1,
-                    None => {
-                        if inside {
-                            vol += 1;
-                        }
-                    }
-                    c => panic!("Unknown option {:?}", c),
-                }
-            }
-        }
+        let (dims, border) = build_map(&self.instructions);
+        let vol = get_volume(&dims, &border);
         output(vol)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let instructions = self
+            .instructions
+            .iter()
+            .map(Instruction::flip)
+            .collect::<Vec<_>>();
+        let (dims, border) = build_map(&instructions);
+        let vol = get_volume(&dims, &border);
+        output(vol)
     }
 }
 
-impl AocDay {
-    fn build_map(&mut self) {
-        let mut cur = (0, 0);
-        let mut min_row = 0;
-        let mut max_row = 0;
-        let mut min_col = 0;
-        let mut max_col = 0;
-        let mut prev = self.instructions[0].direction;
-        self.border.insert(
-            cur,
-            border_symbol(self.instructions.last().unwrap().direction, prev),
-        );
-        for instruction in &self.instructions {
-            self.border
-                .insert(cur, border_symbol(prev, instruction.direction));
-            prev = instruction.direction;
-            for _ in 0..instruction.distance {
-                cur = instruction.direction.delta(&cur);
-                min_row = min_row.min(cur.0);
-                max_row = max_row.max(cur.0);
-                min_col = min_col.min(cur.1);
-                max_col = max_col.max(cur.1);
-                self.border
-                    .insert(cur, border_symbol(prev, instruction.direction));
+fn get_volume(dimensions: &Dimensions, border: &HashMap<(i64, i64), char>) -> i64 {
+    let mut vol = 0;
+    let mut inside = false;
+    let mut cur = '.';
+    for row in dimensions.0..=dimensions.1 {
+        for col in dimensions.2..=dimensions.3 + 1 {
+            match border.get(&(row, col)) {
+                Some('|') => {
+                    vol += 1;
+                    inside = !inside;
+                }
+                Some('F') => {
+                    vol += 1;
+                    inside = !inside;
+                    cur = 'F';
+                }
+                Some('L') => {
+                    vol += 1;
+                    inside = !inside;
+                    cur = 'L';
+                }
+                Some('J') => {
+                    vol += 1;
+                    if cur == 'L' {
+                        inside = !inside;
+                        cur = '.';
+                    }
+                }
+                Some('7') => {
+                    vol += 1;
+                    if cur == 'F' {
+                        inside = !inside;
+                        cur = '.';
+                    }
+                }
+                Some('-') => vol += 1,
+                None => {
+                    if inside {
+                        vol += 1;
+                    }
+                }
+                c => panic!("Unknown option {:?}", c),
             }
         }
-        if cur == (0, 0) {
-            self.border
-                .insert(cur, border_symbol(prev, self.instructions[0].direction));
-        }
-        self.dimensions = (min_row, max_row, min_col, max_col);
     }
+    vol
+}
+
+fn build_map(instructions: &[Instruction]) -> (Dimensions, HashMap<(i64, i64), char>) {
+    let mut cur = (0, 0);
+    let mut min_row = 0;
+    let mut max_row = 0;
+    let mut min_col = 0;
+    let mut max_col = 0;
+    let mut prev = instructions[0].direction;
+    let mut border = HashMap::new();
+    border.insert(
+        (cur.0 as i64, cur.1 as i64),
+        border_symbol(instructions.last().unwrap().direction, prev),
+    );
+    for instruction in instructions {
+        border.insert(
+            (cur.0 as i64, cur.1 as i64),
+            border_symbol(prev, instruction.direction),
+        );
+        prev = instruction.direction;
+        for _ in 0..instruction.distance {
+            cur = instruction.direction.delta(&cur);
+            min_row = min_row.min(cur.0 as i64);
+            max_row = max_row.max(cur.0 as i64);
+            min_col = min_col.min(cur.1 as i64);
+            max_col = max_col.max(cur.1 as i64);
+            border.insert(
+                (cur.0 as i64, cur.1 as i64),
+                border_symbol(prev, instruction.direction),
+            );
+        }
+    }
+    if cur == (0, 0) {
+        border.insert(
+            (cur.0 as i64, cur.1 as i64),
+            border_symbol(prev, instructions[0].direction),
+        );
+    }
+    ((min_row, max_row, min_col, max_col), border)
 }
 
 fn border_symbol(previous: Dir, current: Dir) -> char {
@@ -133,11 +148,38 @@ fn border_symbol(previous: Dir, current: Dir) -> char {
     }
 }
 
+type Dimensions = (i64, i64, i64, i64);
+
 #[derive(Debug, PartialEq, Eq)]
 struct Instruction {
     direction: Dir,
-    distance: i32,
+    distance: i64,
     color: String,
+}
+
+impl Instruction {
+    fn flip(&self) -> Self {
+        let mut c = self
+            .color
+            .trim_start_matches("(#")
+            .trim_end_matches(')')
+            .to_string();
+        let direction = match c.pop() {
+            Some('0') => Dir::East,
+            Some('1') => Dir::South,
+            Some('2') => Dir::West,
+            Some('3') => Dir::North,
+            Some(c) => panic!("Unknown Direction {}", c),
+            None => panic!("Can't convert color to number."),
+        };
+        let distance =
+            i64::from_str_radix(&c, 16).unwrap_or_else(|_| panic!("Can't convert to distance."));
+        Self {
+            direction,
+            distance,
+            color: self.color.clone(),
+        }
+    }
 }
 
 impl FromStr for Instruction {
@@ -215,6 +257,18 @@ U 2 (#7a21e3)";
         day.parse();
         let expected = 62;
         let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part2() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 952408144115_i64;
+        let actual = day.part2()[0].parse().unwrap_or_default();
         assert_eq!(expected, actual);
     }
 }
