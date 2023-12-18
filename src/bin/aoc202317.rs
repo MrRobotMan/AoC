53255ea04e9c218161f9a3617a7cc98a937cb286
David Weiss <dweiss2@gmail.com>
2023-12-17 22:08:49 -0600

    2023 Day 17 Refactor to have path as a separate function

--- src/bin/aoc202317.rs
+++ src/bin/aoc202317.rs
@@ -30,11 +30,22 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
+        output(self.get_path((1, 3)))
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        output("Unsolved")
+    }
+}
+
+impl AocDay {
+    fn get_path(&self, limits: (usize, usize)) -> usize {
         let path = dijkstra(
             &Node {
                 pos: (0, 0),
                 dir: Dir::East,
                 steps: 1,
+                limits,
                 target: (self.map.size.0 - 1, self.map.size.1 - 1),
             },
             &self.map,
@@ -43,15 +54,9 @@ impl Runner for AocDay {
         .iter()
         .map(|n| n.pos)
         .collect::<Vec<_>>();
-        let heat_loss = path
-            .iter()
+        path.iter()
             .skip(1)
-            .fold(0, |acc, node| acc + self.map.grid[node]);
-        output(heat_loss)
-    }
-
-    fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+            .fold(0, |acc, node| acc + self.map.grid[node])
     }
 }
 
@@ -66,6 +71,7 @@ struct Node {
     pos: (usize, usize),
     dir: Dir,
     steps: usize,
+    limits: (usize, usize),
     target: (usize, usize),
 }
 
@@ -93,7 +99,11 @@ impl Searcher for Node {
             .into_iter()
             .zip(steps.into_iter().zip(dirs))
             .filter_map(|(pos, (steps, dir))| {
-                if steps <= 3 && pos != self.pos && dir != dont_go {
+                if self.limits.0 <= steps
+                    && steps <= self.limits.1
+                    && pos != self.pos
+                    && dir != dont_go
+                {
                     let mut node = self.clone();
                     node.pos = pos;
                     node.dir = dir;
@@ -116,75 +126,6 @@ impl Weighted<Node> for Map {
         self.grid[&node.pos]
     }
 }
-
-// impl Map {
-//     fn weight(&self, node: &Node) -> usize {
-//         self.grid[&node.pos]
-//     }
-
-//     fn dijkstra(&self, start: Node) -> Option<Vec<Node>> {
-//         let mut queue: HashSet<Node> = HashSet::new();
-//         let mut dist: HashMap<Node, usize> = HashMap::new();
-//         let mut path: HashMap<Node, Node> = HashMap::new();
-//         let mut index: HashSet<Node> = HashSet::new();
-//         let mut target = None;
-
-//         index.insert(start.clone());
-//         queue.insert(start.clone());
-//         dist.insert(start.clone(), 0);
-
-//         while !queue.is_empty() {
-//             let shortest = queue
-//                 .iter()
-//                 .map(|item| (item, dist.get(item).unwrap()))
-//                 .min_by(|a, b| a.1.cmp(b.1))
-//                 .unwrap()
-//                 .0
-//                 .clone();
-
-//             if shortest.is_done() {
-//                 // Found target. Let's build the path.
-//                 target = Some(shortest);
-//                 break;
-//             }
-
-//             if !queue.remove(&shortest) {
-//                 panic!("Tried to remove shortest from queue but it was not found.")
-//             }
-
-//             for next_move in shortest.moves() {
-//                 let step = if queue.contains(&next_move) {
-//                     next_move
-//                 } else if index.insert(next_move.clone()) {
-//                     dist.insert(next_move.clone(), usize::MAX);
-//                     queue.insert(next_move.clone());
-//                     next_move
-//                 } else {
-//                     continue;
-//                 };
-//                 let cur = dist[&shortest];
-//                 let s = dist[&step];
-//                 let alt = cur + self.weight(&step);
-//                 if alt < s {
-//                     dist.insert(step.clone(), alt);
-//                     path.insert(step.clone(), shortest.clone());
-//                 }
-//             }
-//         }
-
-//         if let Some(end) = target {
-//             let mut found = Vec::new();
-//             found.push(end);
-//             while let Some(node) = path.get(found.last().unwrap()) {
-//                 found.push(node.clone());
-//             }
-//             found.reverse();
-//             Some(found)
-//         } else {
-//             None
-//         }
-//     }
-// }
 
 impl From<Vec<Vec<char>>> for Map {
     fn from(value: Vec<Vec<char>>) -> Self {
@@ -252,7 +193,19 @@ mod tests {
         };
         day.parse();
         let expected = 102;
-        let actual = day.part1()[0].parse::<i32>().unwrap_or_default();
+        let actual = day.get_path((1, 3));
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part2() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 94;
+        let actual = day.get_path((4, 10));
         assert_eq!(expected, actual);
     }
 }
