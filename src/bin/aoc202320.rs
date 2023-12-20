9a80b6ab54f2d279b6e29c5b7aeb2085fe1ef271
David Weiss <dweiss2@gmail.com>
2023-12-20 09:24:25 -0600

    2023 Day 20 Parsing

--- src/bin/aoc202320.rs
+++ src/bin/aoc202320.rs
@@ -1,13 +1,20 @@
+use std::collections::HashMap;
+
 use aoc::runner::{output, run_solution, Runner};
 
 fn main() {
-    let mut day = AocDay{input: "inputs/2023/day20.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/2023/day20.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    modules: HashMap<String, ModuleType>,
+    instructions: Vec<(String, Vec<String>)>,
 }
 
 impl Runner for AocDay {
@@ -16,7 +23,37 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        for line in aoc::read_lines(&self.input) {
+            let (module, connections) = line.split_once(" -> ").unwrap();
+
+            let sender = if let Some(k) = module.strip_prefix('%') {
+                self.modules
+                    .insert(k.into(), ModuleType::FlipFlop(FlipFlop { is_on: false }));
+                k.into()
+            } else if let Some(k) = module.strip_prefix('&') {
+                self.modules
+                    .insert(k.into(), ModuleType::Conjunction(Conjunction::default()));
+                k.into()
+            } else {
+                self.modules.insert(module.into(), ModuleType::Broadcaster);
+                module.into()
+            };
+            self.instructions.push((
+                sender,
+                connections
+                    .split(", ")
+                    .map(|c| c.to_string())
+                    .collect::<Vec<_>>(),
+            ));
+            for (sender, receivers) in &self.instructions {
+                for receiver in receivers {
+                    let module = self.modules.get_mut(receiver);
+                    if let Some(ModuleType::Conjunction(c)) = module {
+                        c.status.insert(sender.clone(), Pulse::Low);
+                    }
+                }
+            }
+        }
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -27,4 +64,70 @@ impl Runner for AocDay {
         output("Unsolved")
     }
 }
-        
+
+trait PulseProcessor {
+    fn process_pulse(&self, pulse: Pulse, sender: &str) -> Option<(Pulse, String)>;
+}
+
+#[derive(Debug, Default)]
+enum Pulse {
+    High,
+    #[default]
+    Low,
+}
+
+#[derive(Default, Debug)]
+struct FlipFlop {
+    is_on: bool,
+}
+
+#[derive(Default, Debug)]
+struct Conjunction {
+    status: HashMap<String, Pulse>,
+}
+
+#[derive(Debug)]
+enum ModuleType {
+    Broadcaster,
+    FlipFlop(FlipFlop),
+    Conjunction(Conjunction),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_version1() {
+        let mut day = AocDay {
+            input: "broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a"
+                .into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 32_000_000;
+        let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part1_version2() {
+        let mut day = AocDay {
+            input: "broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output"
+                .into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 11_687_500;
+        let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
+}
