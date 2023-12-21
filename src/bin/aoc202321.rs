b65972be8070cc1ebb579b4340902edd59005042
David Weiss <dweiss2@gmail.com>
2023-12-21 00:30:43 -0600

    2023 Day 21 Part 2 - Brute force working for small tests. Need to get smrt

--- src/bin/aoc202321.rs
+++ src/bin/aoc202321.rs
@@ -58,31 +58,21 @@ impl Runner for AocDay {
                 .iter()
                 .flat_map(|cell| self.garden.step(cell))
                 .collect();
-
-            if cfg!(test) {
-                for row in 0..self.garden.height {
-                    for col in 0..self.garden.width {
-                        print!(
-                            "{}",
-                            if visited.contains(&(row, col)) {
-                                'O'
-                            } else if self.garden.layout.contains_key(&(row, col)) {
-                                '#'
-                            } else {
-                                '.'
-                            }
-                        )
-                    }
-                    println!();
-                }
-                println!();
-            };
         }
         output(visited.len())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let mut visited = HashSet::new();
+        visited.insert(self.garden.start);
+        for s in 0..self.steps {
+            println!("step {}", s + 1);
+            visited = visited
+                .iter()
+                .flat_map(|cell| self.garden.step_infinite(cell))
+                .collect();
+        }
+        output(visited.len())
     }
 }
 
@@ -113,16 +103,39 @@ impl Garden {
             })
             .collect()
     }
+
+    fn step_infinite(&self, position: &(i64, i64)) -> Vec<(i64, i64)> {
+        [Dir::North, Dir::South, Dir::East, Dir::West]
+            .iter()
+            .filter_map(|dir| {
+                let new_pos = dir.delta(position);
+                let mut check_pos = (new_pos.0 % self.height, new_pos.1 % self.width);
+                if check_pos.0 < 0 {
+                    check_pos.0 += self.height;
+                }
+                if check_pos.1 < 0 {
+                    check_pos.1 += self.width;
+                }
+                if check_pos.0 < 0
+                    || check_pos.0 >= self.height
+                    || check_pos.1 < 0
+                    || check_pos.1 >= self.width
+                    || self.layout.contains_key(&check_pos)
+                {
+                    None
+                } else {
+                    Some(new_pos)
+                }
+            })
+            .collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() {
-        let mut day = AocDay {
-            input: "...........
+    static INPUT: &str = "...........
 .....###.#.
 .###.##..#.
 ..#.#...#..
@@ -132,14 +145,31 @@ mod tests {
 .......##..
 .##.#.####.
 .##..##.##.
-..........."
-                .into(),
+...........";
+
+    #[test]
+    fn test_part1() {
+        let mut day = AocDay {
+            input: INPUT.into(),
             steps: 6,
             ..Default::default()
         };
         day.parse();
         let expected = 16;
         let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part2() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            steps: 50,
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 1594;
+        let actual = day.part2()[0].parse().unwrap_or_default();
         assert_eq!(expected, actual);
     }
 }
