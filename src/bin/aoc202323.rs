0c10a9943b4252202bb1143aeeac7e1e0bbe0ae1
David Weiss <dweiss2@gmail.com>
2023-12-22 23:25:23 -0600

    2023 Day 23 Parsing

--- src/bin/aoc202323.rs
+++ src/bin/aoc202323.rs
@@ -1,13 +1,17 @@
 use aoc::runner::{output, run_solution, Runner};
 
 fn main() {
-    let mut day = AocDay{input: "inputs/2023/day23.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/2023/day23.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    trails: Vec<Vec<Tile>>,
 }
 
 impl Runner for AocDay {
@@ -16,7 +20,10 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        for line in aoc::read_grid(&self.input) {
+            self.trails
+                .push(line.iter().map(|c| c.into()).collect::<Vec<_>>());
+        }
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -27,4 +34,90 @@ impl Runner for AocDay {
         output("Unsolved")
     }
 }
-        
+
+impl AocDay {
+    fn _dump(&self) {
+        for line in self.trails.iter() {
+            for ch in line.iter() {
+                print!(
+                    "{}",
+                    match ch {
+                        Tile::Path => '.',
+                        Tile::Forest => '#',
+                        Tile::SlopeUp => '^',
+                        Tile::SlopeRight => '>',
+                        Tile::SlopeLeft => '<',
+                        Tile::SlopeDown => 'v',
+                    }
+                )
+            }
+            println!();
+        }
+    }
+}
+
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
+enum Tile {
+    #[default]
+    Path, // .
+    Forest,     // #
+    SlopeUp,    // ^
+    SlopeRight, // >
+    SlopeLeft,  // <
+    SlopeDown,  // v
+}
+
+impl From<&char> for Tile {
+    fn from(value: &char) -> Self {
+        match *value {
+            '.' => Self::Path,
+            '#' => Self::Forest,
+            '^' => Self::SlopeUp,
+            '>' => Self::SlopeRight,
+            '<' => Self::SlopeLeft,
+            'v' => Self::SlopeDown,
+            _ => unreachable!("Found unknown character"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT: &str = "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#";
+
+    #[test]
+    fn test_part1() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 94;
+        let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
+}
