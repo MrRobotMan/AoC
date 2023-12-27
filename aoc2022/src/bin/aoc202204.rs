68af6118fab1a5fb6dda2a14c87815dcf8a91cb5
David Weiss <dweiss2@gmail.com>
2023-12-27 12:10:39 -0600

    2022 Days 4 and 5

--- aoc2022/src/bin/aoc202204.rs
+++ aoc2022/src/bin/aoc202204.rs
@@ -1,13 +1,19 @@
+use std::ops::RangeInclusive;
+
 use aoc::runner::{output, run_solution, Runner};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day04.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day04.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    pairs: Vec<(RangeInclusive<u8>, RangeInclusive<u8>)>,
 }
 
 impl Runner for AocDay {
@@ -16,40 +22,40 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        self.pairs = aoc::read_lines(&self.input)
+            .iter()
+            .map(|l| {
+                let (left, right) = l.split_once(',').unwrap();
+                (str_to_range(left), str_to_range(right))
+            })
+            .collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(
+            self.pairs
+                .iter()
+                .filter(|(left, right)| {
+                    (left.contains(right.start()) && left.contains(right.end()))
+                        || (right.contains(left.start()) && right.contains(left.end()))
+                })
+                .count(),
+        )
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(
+            self.pairs
+                .iter()
+                .filter(|(left, right)| {
+                    left.contains(right.start()) || right.contains(left.start())
+                })
+                .count(),
+        )
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    static INPUT: &str = "";      
-
-    #[test]
-    fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
-
-    #[test]
-    fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
-    }
-        
+fn str_to_range(s: &str) -> RangeInclusive<u8> {
+    let (start, end) = s.split_once('-').unwrap();
+    start.parse().unwrap()..=end.parse().unwrap()
+}
--- aoc2022/src/bin/aoc202205.rs
+++ aoc2022/src/bin/aoc202205.rs
@@ -1,13 +1,18 @@
 use aoc::runner::{output, run_solution, Runner};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day05.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day05.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    layout: Layout,
+    instructions: Vec<Instruction>,
 }
 
 impl Runner for AocDay {
@@ -16,15 +21,109 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        let lines = aoc::lines(self.input.clone());
+        let (layout, insructions) = lines.split_once("\n\n").unwrap();
+        self.layout = Layout::new(layout, 4, 1);
+        self.instructions = insructions.lines().map(Instruction::new).collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let mut layout = self.layout.clone();
+        for inst in &self.instructions {
+            layout.process_instruction(inst);
+        }
+        output(
+            layout
+                .stacks
+                .iter()
+                .map(|stack| stack.last().unwrap_or(&' '))
+                .collect::<String>(),
+        )
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let mut layout = self.layout.clone();
+        for inst in &self.instructions {
+            layout.process_many(inst);
+        }
+        output(
+            layout
+                .stacks
+                .iter()
+                .map(|stack| stack.last().unwrap_or(&' '))
+                .collect::<String>(),
+        )
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct Layout {
+    stacks: Vec<Vec<char>>,
+}
+
+impl Layout {
+    fn new(data: &str, step: usize, start: usize) -> Self {
+        let mut rows: Vec<&str> = data.trim_end().split('\n').collect();
+        let stack_count = rows
+            .pop() // Last row
+            .unwrap() // Only would panic if the vec was empty
+            .split_ascii_whitespace() // Get all the column numbers
+            .last()
+            .unwrap() // Only would panic if the vec was empty
+            .parse::<usize>()
+            .unwrap();
+        let mut stacks: Vec<Vec<char>> = vec![Vec::new(); stack_count];
+        for row in rows {
+            let mut chrs = row.chars();
+            chrs.nth(start - 1); // move the iterator to the correct column
+            for (stack, chr) in chrs.step_by(step).enumerate() {
+                if chr == ' ' {
+                    continue;
+                }
+                stacks[stack].insert(0, chr);
+            }
+        }
+
+        Self { stacks }
+    }
+
+    fn process_instruction(&mut self, instruction: &Instruction) {
+        for _ in 0..instruction.qty {
+            if let Some(chr) = self.stacks[instruction.from].pop() {
+                self.stacks[instruction.to].push(chr);
+            }
+        }
+    }
+
+    fn process_many(&mut self, instruction: &Instruction) {
+        let mut gathered = Vec::new();
+        for _ in 0..instruction.qty {
+            if let Some(chr) = self.stacks[instruction.from].pop() {
+                gathered.push(chr);
+            }
+        }
+        gathered.reverse();
+        for chr in gathered {
+            self.stacks[instruction.to].push(chr);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Instruction {
+    from: usize,
+    to: usize,
+    qty: usize,
+}
+
+impl Instruction {
+    fn new(data: &str) -> Self {
+        // instruction is in the from "move _ from _ to _"
+        let processed: Vec<&str> = data.split_ascii_whitespace().collect();
+        let qty = processed[1].parse::<usize>().unwrap();
+        let from = processed[3].parse::<usize>().unwrap() - 1;
+        let to = processed[5].parse::<usize>().unwrap() - 1;
+        Self { from, to, qty }
     }
 }
 
@@ -32,24 +131,50 @@ impl Runner for AocDay {
 mod tests {
     use super::*;
 
-    static INPUT: &str = "";      
+    static INPUT: &str = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     #[test]
-    fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
-
-    #[test]
-    fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
+    fn make_layout() {
+        let expected = vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']];
+        let actual = Layout::new("    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n", 4, 1);
+        assert_eq!(actual.stacks, expected);
     }
-        
+
+    #[test]
+    fn instruction() {
+        let actual = Instruction::new("move 1 from 2 to 8");
+        assert_eq!((actual.from, actual.to, actual.qty), (1, 7, 1));
+    }
+
+    #[test]
+    fn part_one() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = "CMZ";
+        let actual = &day.part1()[0];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn part_two() {
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = "MCD";
+        let actual = &day.part2()[0];
+        assert_eq!(actual, expected);
+    }
+}
