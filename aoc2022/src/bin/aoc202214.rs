9841e598e011f19e7e75cc5330b78304fa35aa01
David Weiss <dweiss2@gmail.com>
2023-12-28 21:09:35 -0600

    2022 Days 14 & 15

--- aoc2022/src/bin/aoc202214.rs
+++ aoc2022/src/bin/aoc202214.rs
@@ -1,13 +1,17 @@
 use aoc::runner::{output, run_solution, Runner};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day14.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day14.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    cave: Cave,
 }
 
 impl Runner for AocDay {
@@ -16,40 +20,151 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        let mut min_col = usize::MAX;
+        let mut max_col = 0;
+        let mut max_row = 0;
+        let mut rocks = Vec::new();
+        for path in aoc::read_lines(&self.input) {
+            let mut v = Vec::new();
+            for turn in path.split(" -> ") {
+                if let Some((col, row)) = turn.split_once(',') {
+                    let col = col.parse().unwrap();
+                    let row = row.parse().unwrap();
+                    max_col = max_col.max(col);
+                    min_col = min_col.min(col);
+                    max_row = max_row.max(row);
+                    v.push((col, row))
+                }
+            }
+            rocks.push(v);
+        }
+        self.cave = Cave::new(&rocks, (min_col, max_col), max_row, 500);
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        self.cave.drop_sand();
+        output(self.cave.grains)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        self.cave.set_floor();
+        self.cave.drop_sand();
+        output(self.cave.grains)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Default)]
+struct Cave {
+    nodes: Vec<Vec<char>>,
+    sand_col: usize,
+    width: usize,
+    height: usize,
+    grains: i32,
+}
 
-    static INPUT: &str = "";      
-
-    #[test]
-    fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
+impl Cave {
+    fn new(
+        rocks: &[Vec<(usize, usize)>],
+        col_range: (usize, usize),
+        height: usize,
+        sand_col: usize,
+    ) -> Self {
+        let col_offset = col_range.0 - 1;
+        let sand_col = sand_col - col_offset;
+        let width = col_range.1 - col_range.0 + 3;
+        let mut nodes = vec![vec!['.'; width]; height + 1];
+        for rock in rocks.iter() {
+            for cur in 1..rock.len() {
+                let prev = rock[cur - 1];
+                let cur = rock[cur];
+                if cur.0 as i32 - prev.0 as i32 != 0 {
+                    // Moving horizontal
+                    for col in ((cur.0).min(prev.0))..=((prev.0).max(cur.0)) {
+                        nodes[cur.1][col - col_offset] = '#';
+                    }
+                } else {
+                    // Moving vertical
+                    for row in nodes
+                        .iter_mut()
+                        .take((prev.1).max(cur.1))
+                        .skip((prev.1).min(cur.1))
+                    {
+                        row[cur.0 - col_offset] = '#';
+                    }
+                    // for row in ((cur.1).min(prev.1))..=((prev.1).max(cur.1)) {
+                    //     nodes[row][cur.0 - col_offset] = '#';
+                    // }
+                }
+            }
         }
+        nodes[0][sand_col] = '+';
 
-    #[test]
-    fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
+        Self {
+            nodes,
+            sand_col,
+            width,
+            height,
+            grains: 0,
         }
     }
-        
+
+    fn drop_sand(&mut self) {
+        let mut grain_location = (0, self.sand_col);
+        while grain_location.0 < self.height
+            && grain_location.1 > 0
+            && grain_location.1 < self.width
+        {
+            grain_location = (0, self.sand_col);
+            while grain_location.0 < self.height
+                && grain_location.1 > 0
+                && grain_location.1 < self.width
+            {
+                let row_below = (
+                    self.nodes[grain_location.0 + 1][grain_location.1 - 1],
+                    self.nodes[grain_location.0 + 1][grain_location.1],
+                    self.nodes[grain_location.0 + 1][grain_location.1 + 1],
+                );
+                match row_below {
+                    (_, '.', _) => grain_location.0 += 1, // Fall down
+                    ('.', '#', _) | ('.', 'o', _) => {
+                        // Fall down left
+                        grain_location.1 -= 1;
+                        grain_location.0 += 1
+                    }
+                    (_, '#', '.') | (_, 'o', '.') => {
+                        // Fall down right
+                        grain_location.1 += 1;
+                        grain_location.0 += 1
+                    }
+                    _ => {
+                        // Can't fall. Grain stays here.
+                        self.nodes[grain_location.0][grain_location.1] = 'o';
+                        self.grains += 1;
+                        if grain_location.0 == 0 {
+                            return;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn set_floor(&mut self) {
+        self.height += 2;
+        let cur = self.width;
+        let center = self.sand_col;
+        self.width = 2 * (self.height + 1) + 1;
+        self.sand_col = self.height + 1;
+        let shift_right = self.height + 1 - center;
+        let add_to_left = self.height + center + 2 - cur;
+        for row in self.nodes.iter_mut() {
+            let mut new = vec!['.'; shift_right];
+            new.extend(row.clone());
+            new.extend(vec!['.'; add_to_left]);
+            *row = new;
+        }
+        self.nodes.push(vec!['.'; self.width]);
+        self.nodes.push(vec!['#'; self.width]);
+    }
+}
--- aoc2022/src/bin/aoc202215.rs
+++ aoc2022/src/bin/aoc202215.rs
@@ -1,13 +1,21 @@
 use aoc::runner::{output, run_solution, Runner};
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day15.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day15.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    beacons: HashMap<(i32, i32), i32>,
+    target_row: i32,
+    target_range: (i32, i32),
 }
 
 impl Runner for AocDay {
@@ -16,40 +24,80 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        for line in aoc::read_lines(&self.input) {
+            if let Some((sensor, beacon)) = line.split_once(':') {
+                let sensor = extract_coords(sensor);
+                let beacon = extract_coords(beacon);
+                self.beacons.insert(sensor, get_distance(&sensor, &beacon));
+            };
+        }
+        self.target_row = 2_000_000;
+        self.target_range = (0, 4_000_000);
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let mut res = 0;
+        let mut crossings = HashSet::new();
+        for ((x, y), distance) in self.beacons.iter() {
+            let delta = distance - get_distance(&(*x, *y), &(*x, self.target_row));
+            if delta < 0 {
+                continue;
+            } else {
+                crossings.insert((x - delta, x + delta));
+            }
+        }
+
+        let mut crossings = crossings.iter().cloned().collect::<Vec<(i32, i32)>>();
+        crossings.sort();
+        let (mut start, mut end) = crossings[0];
+        for (s, e) in crossings.iter() {
+            if *s > end {
+                res += end - start;
+                start = *s;
+            }
+            end = end.max(*e);
+        }
+        res += end - start;
+        output(res)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let scale = 4_000_000;
+        for row in self.target_range.0..self.target_range.1 {
+            let mut crossings = HashSet::new();
+            for ((x, y), distance) in self.beacons.iter() {
+                let delta = distance - get_distance(&(*x, *y), &(*x, row));
+                if delta < 0 {
+                    continue;
+                } else {
+                    crossings.insert((x - delta, x + delta));
+                }
+            }
+
+            let mut crossings = crossings.iter().cloned().collect::<Vec<(i32, i32)>>();
+            crossings.sort();
+            let mut end = crossings[0].0;
+            for (s, e) in crossings.iter() {
+                if s > &end {
+                    return output(scale * (*s as i64 - 1) + row as i64);
+                }
+                end = end.max(*e);
+            }
+        }
+        output(scale)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn get_distance(start: &(i32, i32), other: &(i32, i32)) -> i32 {
+    (start.0 - other.0).abs() + (start.1 - other.1).abs()
+}
 
-    static INPUT: &str = "";      
-
-    #[test]
-    fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
-
-    #[test]
-    fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
-    }
-        
+fn extract_coords(s: &str) -> (i32, i32) {
+    let re = Regex::new(r"(?:=)(-?\d+)").unwrap();
+    let v = re
+        .captures_iter(s)
+        .filter_map(|capture| capture.get(1))
+        .map(|ma| ma.as_str().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    (v[0], v[1])
+}
