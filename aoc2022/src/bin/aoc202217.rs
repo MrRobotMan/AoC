f8033a9657d437eadd3264d4dfa74221faf7a6a9
David Weiss <dweiss2@gmail.com>
2023-12-29 23:03:14 -0600

    2022 Day 17 Part 2

--- aoc2022/src/bin/aoc202217.rs
+++ aoc2022/src/bin/aoc202217.rs
@@ -1,4 +1,8 @@
-use std::collections::HashSet;
+use std::{
+    collections::{HashMap, HashSet},
+    iter::Cycle,
+    slice::Iter,
+};
 
 use aoc::runner::{output, run_solution, Runner};
 
@@ -26,48 +30,87 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut pattern = self.pattern.iter().cycle();
         let mut rocks = ROCKS.iter().cycle();
-        let mut height = 3;
+        let mut height = 0;
         let mut placed = HashSet::new();
-        'next_rock: for _ in 0..2022 {
-            let cur = rocks.next().unwrap();
-            let mut base = (2, height);
-            // _show(&cur.points(base), &placed, height);
-            loop {
-                base = cur.slide(*pattern.next().unwrap(), base, &placed);
-                if let Some(shifted) = cur.drop(base, &placed) {
-                    base = shifted;
-                } else {
-                    placed.extend(cur.points(base));
-                    height = height.max(cur.top(base) + 4);
-                    continue 'next_rock;
-                };
-            }
+        let mut pattern_index = 0;
+        for _ in 0..2022 {
+            let (h, _, idx, _) = self.drop_rock(height, &mut rocks, pattern_index, &mut placed);
+            pattern_index = idx;
+            height = height.max(h);
         }
-        output(height - 3)
+        output(height)
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let total_drops = 1_000_000_000_000_usize;
+        let mut rocks = ROCKS.iter().cycle();
+        let mut height = 0;
+        let mut placed = HashSet::new();
+        let mut tower = vec![vec![false; 7]];
+        let mut drop = 0;
+        let mut drops = HashMap::new();
+        let mut pattern_index = 0;
+        let (start, cycle, start_height, cycle_height) = loop {
+            drop += 1;
+            let (h, rock, jet, points) =
+                self.drop_rock(height, &mut rocks, pattern_index, &mut placed);
+            pattern_index = jet;
+            height = height.max(h);
+            for _ in 0..(height - tower.len()) {
+                tower.push(vec![false; 7]);
+            }
+            for p in points {
+                tower[p.1][p.0] = true;
+            }
+            if height <= 4 {
+                continue;
+            }
+            if let Some(prev) = drops.insert(
+                (tower[(height - 4)..height].to_vec(), rock, jet),
+                (drop, height),
+            ) {
+                break (prev.0, drop - prev.0, prev.1, height - prev.1);
+            };
+        };
+        let cycles = (total_drops - start) / cycle;
+        let height_at_cycle_end = start_height + cycles * cycle_height;
+        let mut remaining_height = height;
+        let remaining = (total_drops - start) % cycle;
+        for _ in 0..remaining {
+            let (h, _, idx, _) =
+                self.drop_rock(remaining_height, &mut rocks, pattern_index, &mut placed);
+            pattern_index = idx;
+            remaining_height = remaining_height.max(h);
+        }
+        output(height_at_cycle_end + remaining_height - height)
     }
 }
 
-fn _show(cur: &HashSet<(isize, isize)>, placed: &HashSet<(isize, isize)>, height: isize) {
-    for row in 0..=height {
-        print!("|");
-        for col in 0..7 {
-            if cur.contains(&(col, height - row)) {
-                print!("@");
-            } else if placed.contains(&(col, height - row)) {
-                print!("#");
+impl AocDay {
+    fn drop_rock(
+        &self,
+        height: usize,
+        rocks: &mut Cycle<Iter<Rock>>,
+        mut pattern_index: usize,
+        placed: &mut HashSet<(usize, usize)>,
+    ) -> (usize, Rock, usize, HashSet<(usize, usize)>) {
+        let cur = rocks.next().unwrap();
+        let mut base = (2, height + 3);
+        loop {
+            // if height >
+            let pat = self.pattern[pattern_index];
+            base = cur.slide(pat, base, placed);
+            pattern_index = (pattern_index + 1) % self.pattern.len();
+            if let Some(shifted) = cur.drop(base, placed) {
+                base = shifted;
             } else {
-                print!(".");
+                let points = cur.points(base);
+                placed.extend(points.clone());
+                return (cur.top(base) + 1, *cur, pattern_index, points);
             };
         }
-        println!("|");
     }
-    println!("+-------+\n");
 }
 
 const ROCKS: [Rock; 5] = [
@@ -78,6 +121,7 @@ const ROCKS: [Rock; 5] = [
     Rock::Square,
 ];
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Rock {
     Horizontal,
     Cross,
@@ -87,7 +131,7 @@ enum Rock {
 }
 
 impl Rock {
-    fn points(&self, base: (isize, isize)) -> HashSet<(isize, isize)> {
+    fn points(&self, base: (usize, usize)) -> HashSet<(usize, usize)> {
         let x = base.0;
         let y = base.1;
         match self {
@@ -111,16 +155,16 @@ impl Rock {
         }
     }
 
-    fn top(&self, base: (isize, isize)) -> isize {
+    fn top(&self, base: (usize, usize)) -> usize {
         self.points(base).iter().map(|p| p.1).max().unwrap()
     }
 
     fn slide(
         &self,
         dir: char,
-        base: (isize, isize),
-        placed: &HashSet<(isize, isize)>,
-    ) -> (isize, isize) {
+        base: (usize, usize),
+        placed: &HashSet<(usize, usize)>,
+    ) -> (usize, usize) {
         let points = self.points(base);
         let left = points.iter().map(|p| p.0).min().unwrap();
         let right = points.iter().map(|p| p.0).max().unwrap();
@@ -137,11 +181,11 @@ impl Rock {
         };
         let shifted = points
             .iter()
-            .map(|p| (p.0 + offset, p.1))
+            .map(|p| ((p.0 as isize + offset) as usize, p.1))
             .collect::<HashSet<_>>();
         if placed.is_empty() || shifted.intersection(placed).count() == 0 {
             // Didn't hit anything.
-            (base.0 + offset, base.1)
+            ((base.0 as isize + offset) as usize, base.1)
         } else {
             base
         }
@@ -149,9 +193,9 @@ impl Rock {
 
     fn drop(
         &self,
-        base: (isize, isize),
-        placed: &HashSet<(isize, isize)>,
-    ) -> Option<(isize, isize)> {
+        base: (usize, usize),
+        placed: &HashSet<(usize, usize)>,
+    ) -> Option<(usize, usize)> {
         let points = self.points(base);
         let bottom = points.iter().map(|p| p.1).min().unwrap();
 
@@ -198,7 +242,7 @@ mod tests {
             ..Default::default()
         };
         day.parse();
-        let expected = 0;
+        let expected = 1_514_285_714_288_usize;
         let actual = day.part2()[0].parse().unwrap_or_default();
         assert_eq!(expected, actual);
     }
