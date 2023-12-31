2a044b47cb404d15900ed5b0f6e6b4015542ecef
David Weiss <dweiss2@gmail.com>
2023-12-30 22:45:23 -0600

    2022 Day 20 Parsing

--- aoc2022/src/bin/aoc202220.rs
+++ aoc2022/src/bin/aoc202220.rs
@@ -1,13 +1,17 @@
 use aoc::runner::{output, run_solution, Runner};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day20.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day20.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    encrypted_coordinates: Vec<i64>,
 }
 
 impl Runner for AocDay {
@@ -16,7 +20,7 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        self.encrypted_coordinates = aoc::read_numbers(&self.input);
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -32,24 +36,35 @@ impl Runner for AocDay {
 mod tests {
     use super::*;
 
-    static INPUT: &str = "";      
+    static INPUT: &str = "1
+2
+-3
+3
+-2
+0
+4";
 
     #[test]
     fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 3;
+        let actual = day.part1()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
+    }
 
     #[test]
     fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 0;
+        let actual = day.part2()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
     }
-        
+}
