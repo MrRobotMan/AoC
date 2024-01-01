160a728b261c57e74bca0a4610daa9b60c6217f6
David Weiss <dweiss2@gmail.com>
2023-12-31 23:34:38 -0600

    2022 Day 21 Part 2

--- aoc2022/src/bin/aoc202221.rs
+++ aoc2022/src/bin/aoc202221.rs
@@ -36,13 +36,83 @@ impl Runner for AocDay {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        output("Unsolved")
+        let (left, right) = if let Monkey::Operation(left, _, right) = &self.monkeys["root"] {
+            (left, right)
+        } else {
+            panic!("Root is constant");
+        };
+        let mut path = self.find_human("root").unwrap();
+        path.reverse();
+        let total = if left == &path[1] {
+            self.monkeys[right].process(&self.monkeys)
+        } else {
+            self.monkeys[left].process(&self.monkeys)
+        };
+        output(self.adjust_total(&path, 1, total))
     }
 }
 
+impl AocDay {
+    fn find_human(&self, node: &str) -> Option<Vec<String>> {
+        if node == "humn" {
+            return Some(vec!["humn".into()]);
+        }
+
+        if let Monkey::Operation(ref left, _, ref right) = self.monkeys[node] {
+            if let Some(mut vec) = self.find_human(left) {
+                vec.push(node.into());
+                return Some(vec);
+            }
+            if let Some(mut vec) = self.find_human(right) {
+                vec.push(node.into());
+                return Some(vec);
+            }
+        }
+
+        None
+    }
+
+    fn adjust_total(&self, path: &[String], idx: usize, total: i64) -> i64 {
+        match self.monkeys[&path[idx]] {
+            Monkey::Constant(_) => total,
+            Monkey::Operation(ref lhs, ope, ref rhs) => {
+                let left = self.monkeys[lhs].process(&self.monkeys);
+                let right = self.monkeys[rhs].process(&self.monkeys);
+                let adjustment = if lhs == &path[idx + 1] {
+                    // A = x + y => x = A - y
+                    // A = x - y => x = A + y
+                    // A = x * y => x = A / y
+                    // A = x / y => x = A * y
+                    match ope.0 {
+                        '+' => total - right,
+                        '-' => total + right,
+                        '*' => total / right,
+                        '/' => total * right,
+                        _ => unreachable!(),
+                    }
+                } else {
+                    match ope.0 {
+                        // A = x + y => y = A - x
+                        // A = x - y => y = x - A
+                        // A = x * y => y = A / x
+                        // A = x / y => y = x / A
+                        '+' => total - left,
+                        '-' => left - total,
+                        '*' => total / left,
+                        '/' => left / total,
+                        _ => unreachable!(),
+                    }
+                };
+                self.adjust_total(path, idx + 1, adjustment)
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
 enum Monkey {
     Constant(i64),
-    Operation(String, Box<dyn Fn(i64, i64) -> i64>, String),
+    Operation(String, (char, fn(i64, i64) -> i64), String),
 }
 
 impl Monkey {
@@ -50,7 +120,7 @@ impl Monkey {
         match self {
             Self::Constant(c) => *c,
             Self::Operation(lhs, ope, rhs) => {
-                ope(monkeys[lhs].process(monkeys), monkeys[rhs].process(monkeys))
+                ope.1(monkeys[lhs].process(monkeys), monkeys[rhs].process(monkeys))
             }
         }
     }
@@ -63,16 +133,31 @@ impl<T: AsRef<str>> From<T> for Monkey {
             1 => Self::Constant(values[0].parse().unwrap()),
             3 => Self::Operation(
                 values[0].into(),
-                Box::new(match values[1] {
-                    "+" => |lhs, rhs| lhs + rhs,
-                    "-" => |lhs, rhs| lhs - rhs,
-                    "*" => |lhs, rhs| lhs * rhs,
-                    "/" => |lhs, rhs| lhs / rhs,
-                    _ => unreachable!("Unknown operation"),
-                }),
+                {
+                    let c = values[1].chars().next().unwrap();
+                    (
+                        c,
+                        match c {
+                            '+' => |lhs, rhs| lhs + rhs,
+                            '-' => |lhs, rhs| lhs - rhs,
+                            '*' => |lhs, rhs| lhs * rhs,
+                            '/' => |lhs, rhs| lhs / rhs,
+                            _ => unreachable!("Unknown operation"),
+                        },
+                    )
+                },
                 values[2].into(),
             ),
             _ => unreachable!("Value has the wrong number of parts"),
+        }
+    }
+}
+
+impl std::fmt::Debug for Monkey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Monkey::Constant(c) => write!(f, "{c}"),
+            Monkey::Operation(lhs, (ope, _), rhs) => write!(f, "{lhs} {ope} {rhs}"),
         }
     }
 }
@@ -116,7 +201,7 @@ hmdt: 32";
             ..Default::default()
         };
         day.parse();
-        let expected = 0;
+        let expected = 301;
         let actual = day.part2()[0].parse().unwrap_or_default();
         assert_eq!(expected, actual);
     }
