9d4fadb523de4ff2a31dd26995cb6d90a68ae2f3
David Weiss <dweiss2@gmail.com>
2024-01-04 09:18:14 -0600

    2022 Day 25 Parsing

--- aoc2022/src/bin/aoc202225.rs
+++ aoc2022/src/bin/aoc202225.rs
@@ -1,13 +1,17 @@
 use aoc::runner::{output, run_solution, Runner};
 
 pub fn main() {
-    let mut day = AocDay{input: "inputs/day25.txt".into(), ..Default::default()};
+    let mut day = AocDay {
+        input: "inputs/day25.txt".into(),
+        ..Default::default()
+    };
     run_solution(&mut day);
 }
 
 #[derive(Default)]
 struct AocDay {
     input: String,
+    burners: Vec<Snafu>,
 }
 
 impl Runner for AocDay {
@@ -16,7 +20,10 @@ impl Runner for AocDay {
     }
 
     fn parse(&mut self) {
-        // Parse the input
+        self.burners = aoc::read_lines(&self.input)
+            .iter()
+            .map(|s| s.into())
+            .collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
@@ -28,28 +35,72 @@ impl Runner for AocDay {
     }
 }
 
+#[derive(Debug)]
+struct Snafu {
+    value: i64,
+}
+
+impl<T: AsRef<str>> From<T> for Snafu {
+    fn from(value: T) -> Self {
+        let value = value
+            .as_ref()
+            .chars()
+            .rev()
+            .enumerate()
+            .fold(0, |acc, (pow, val)| {
+                acc + (match val {
+                    '=' => -2,
+                    '-' => -1,
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    _ => unreachable!("Unknown val"),
+                }) * 5_i64.pow(pow as u32)
+            });
+        Self { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    static INPUT: &str = "";      
+    static INPUT: &str = "1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122";
 
     #[test]
     fn test_part1() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part1()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let _total = 4890;
+        let expected = "2=-10";
+        let actual = &day.part1()[0];
+        assert_eq!(expected, actual);
+    }
 
     #[test]
     fn test_part2() {
-            let mut day = AocDay{input: INPUT.into(), ..Default::default()};
-            day.parse();
-            let expected = 0;
-            let actual = day.part2()[0].parse().unwrap_or_default();
-            assert_eq!(expected, actual);
-        }
+        let mut day = AocDay {
+            input: INPUT.into(),
+            ..Default::default()
+        };
+        day.parse();
+        let expected = 0;
+        let actual = day.part2()[0].parse().unwrap_or_default();
+        assert_eq!(expected, actual);
     }
-        
+}
