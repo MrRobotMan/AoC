98da894f1682e24d6d48d5c2fac32dd0ebeb5fe9
David Weiss <dweiss2@gmail.com>
2024-05-04 22:09:26 -0500

    2021 Day 1 Part 1

--- aoc2021/src/aoc202101.rs
+++ aoc2021/src/aoc202101.rs
@@ -1,8 +1,12 @@
-use aoc::runner::{output, Runner};
+use aoc::{
+    read_numbers,
+    runner::{output, Runner},
+};
 
 #[derive(Default)]
 pub struct AocDay {
     input: String,
+    measurements: Vec<(usize, Delta)>,
 }
 
 impl AocDay {
@@ -21,13 +25,40 @@ impl Runner for AocDay {
 
     fn parse(&mut self) {
         // Parse the input
+        let measurements = read_numbers(&self.input);
+        self.measurements
+            .push((measurements[0], Delta::NotApplicable));
+        for (idx, depth) in measurements.iter().skip(1).enumerate() {
+            self.measurements.push((
+                *depth,
+                match depth.cmp(&self.measurements[idx].0) {
+                    std::cmp::Ordering::Less => Delta::Decrease,
+                    std::cmp::Ordering::Equal => Delta::NoChange,
+                    std::cmp::Ordering::Greater => Delta::Increase,
+                },
+            ))
+        }
     }
 
     fn part1(&mut self) -> Vec<String> {
-        output("Unsolved")
+        output(self.measurements.iter().fold(0, |acc, (_, delta)| {
+            acc + if matches!(delta, Delta::Increase) {
+                1
+            } else {
+                0
+            }
+        }))
     }
 
     fn part2(&mut self) -> Vec<String> {
         output("Unsolved")
     }
+}
+
+#[derive(Debug)]
+enum Delta {
+    NoChange,
+    Increase,
+    Decrease,
+    NotApplicable,
 }
--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::{env, time::Instant};
 
-use aoc::runner::run_solution;
+use aoc::runner::{run_solution, Runner};
 
 mod aoc202101;
 
@@ -8,25 +8,32 @@ mod aoc202101;
 mod tests;
 
 fn main() {
-    let day01 = aoc202101::AocDay::new("inputs/day01.txt");
-    let mut days = [day01];
+    let mut day01 = aoc202101::AocDay::new("aoc2021/inputs/day01.txt");
+    let mut days: Vec<&mut dyn Runner> = vec![&mut day01];
     let len = days.len() - 1;
     match get_args() {
         Some(0) => {
             // Run all days
+            let start = Instant::now();
             for selected in days.iter_mut() {
-                run_solution(selected);
+                run_solution(*selected);
             }
+            let duration = start.elapsed().as_millis();
+            let millis = duration % 1000;
+            let seconds = duration / 1000;
+            let minutes = seconds / 60;
+            let seconds = seconds % 60;
+            println!("\nTotal: {minutes:3}:{seconds:02}.{millis:03}");
         }
         Some(d) => {
             // Run selected day
             let selected = &mut days[(d - 1).min(len)];
-            run_solution(selected)
+            run_solution(*selected);
         }
         None => {
             // Run last day
             let selected = &mut days[len];
-            run_solution(selected);
+            run_solution(*selected);
         }
     };
 }
