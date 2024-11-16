a07b0b64d38108efd531e46ffa20b32ebebc5b97
David Weiss <dweiss2@gmail.com>
2024-11-16 11:21:19 -0600

    2021 day 5 part 2

--- aoc2021/src/aoc202105.rs
+++ aoc2021/src/aoc202105.rs
@@ -8,8 +8,8 @@ use aoc::{
 #[derive(Default)]
 pub struct AocDay {
     pub(crate) input: String,
-    vent_lines: Vec<((u32, u32), (u32, u32))>,
-    vents: HashMap<(u32, u32), usize>,
+    vent_lines: Vec<((i32, i32), (i32, i32))>,
+    vents: HashMap<(i32, i32), usize>,
 }
 
 impl AocDay {
@@ -50,7 +50,8 @@ impl Runner for AocDay {
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        self.add_diagonals();
+        output(self.vents.values().filter(|v| **v > 1).count())
     }
 }
 
@@ -58,8 +59,8 @@ impl AocDay {
     fn count_rows_and_columns(&mut self) {
         for (start, end) in &self.vent_lines {
             if start.0 == end.0 {
-                let min = (start.1).min(end.1);
-                let max = (start.1).max(end.1);
+                let min = start.1.min(end.1);
+                let max = start.1.max(end.1);
                 for col in min..=max {
                     self.vents
                         .entry((start.0, col))
@@ -67,11 +68,29 @@ impl AocDay {
                         .or_insert(1);
                 }
             } else if start.1 == end.1 {
-                let min = (start.0).min(end.0);
-                let max = (start.0).max(end.0);
+                let min = start.0.min(end.0);
+                let max = start.0.max(end.0);
                 for row in min..=max {
                     self.vents
                         .entry((row, start.1))
+                        .and_modify(|v| *v += 1)
+                        .or_insert(1);
+                }
+            }
+        }
+    }
+
+    fn add_diagonals(&mut self) {
+        for (start, end) in &self.vent_lines {
+            if start.0 != end.0 && start.1 != end.1 {
+                let x_steps = end.0 - start.0;
+                let y_steps = end.1 - start.1;
+                let x_step = x_steps.signum();
+                let y_step = y_steps.signum();
+                assert!(x_steps.abs() == y_steps.abs());
+                for step in 0..=x_steps.abs() {
+                    self.vents
+                        .entry((start.0 + x_step * step, start.1 + y_step * step))
                         .and_modify(|v| *v += 1)
                         .or_insert(1);
                 }
@@ -102,5 +121,6 @@ mod test {
             ..Default::default()
         };
         assert_eq!("5", &day.part1());
+        assert_eq!("12", &day.part2());
     }
 }
