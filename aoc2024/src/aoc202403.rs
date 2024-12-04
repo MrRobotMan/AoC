17783945cce9cb349e1ce6413b690fac95d01a79
David Weiss <dweiss2@gmail.com>
2024-12-03 18:51:35 -0600

    2024 Day 3 Part 2

--- aoc2024/src/aoc202403.rs
+++ aoc2024/src/aoc202403.rs
@@ -32,14 +32,32 @@ impl Runner for AocDay {
         let re = Regex::new(r#"mul\((\d+),(\d+)\)"#).unwrap();
         output(
             re.captures_iter(&self.data)
-                .map(|c| c[1].parse::<i64>().unwrap() * c[2].parse::<i64>().unwrap())
+                .map(|c| mult(&c[1], &c[2]))
                 .sum::<i64>(),
         )
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        let mut enabled = true;
+        let re = Regex::new(r#"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"#).unwrap();
+        let mut total = 0;
+        for cap in re.captures_iter(&self.data) {
+            match &cap[0] {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => {
+                    if enabled {
+                        total += mult(&cap[1], &cap[2]);
+                    }
+                }
+            }
+        }
+        output(total)
     }
+}
+
+fn mult(left: &str, right: &str) -> i64 {
+    left.parse::<i64>().unwrap() * right.parse::<i64>().unwrap()
 }
 
 #[cfg(test)]
@@ -55,5 +73,17 @@ mod test {
         let expected = "161";
         let actual = day.part1();
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_example2() {
+        let mut day = AocDay {
+            input: String::new(),
+            data: "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5)))"
+                .into(),
+        };
+        let expexted = "48";
+        let actual = day.part2();
+        assert_eq!(expexted, actual);
     }
 }
