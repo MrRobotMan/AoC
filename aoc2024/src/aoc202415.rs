8f20402d486e701f761870dc5ad0f590f564955f
David Weiss <dweiss2@gmail.com>
2024-12-15 11:57:44 -0600

    2024 Day 15 Part 2

--- aoc2024/src/aoc202415.rs
+++ aoc2024/src/aoc202415.rs
@@ -39,15 +39,19 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> String {
-        let mut warehouse: Warehouse = self.warehouse.as_str().into();
+        let mut warehouse = Warehouse::new(&self.warehouse, false);
         for inst in &self.instructions {
-            warehouse.step(*inst, false);
+            warehouse.step(*inst);
         }
         output(warehouse.goods_score())
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        let mut warehouse = Warehouse::new(&self.warehouse, true);
+        for inst in &self.instructions {
+            warehouse.step(*inst);
+        }
+        output(warehouse.goods_score())
     }
 }
 
@@ -60,48 +64,66 @@ struct Warehouse {
 }
 
 impl Warehouse {
-    fn valid_move_small(&self, inst: Vec2D<i64>) -> bool {
-        let mut next_move = self.robot + inst;
-        while let Some(chr) = self.layout.get(&next_move) {
-            if *chr == '#' {
-                return false;
+    fn step(&mut self, inst: Vec2D<i64>) {
+        let mut replace = vec![];
+        let mut next_move = vec![self.robot + inst];
+        while let Some(next) = next_move.pop() {
+            match (self.layout.get(&next), inst) {
+                (None, _) => (),
+                (Some('#'), _) => return, // Would hit a wall. Leave.
+                // Up / down large box left side. Add the right side.
+                (Some('['), Vec2D(_, 0)) => {
+                    next_move.push(next + inst);
+                    next_move.push(next + inst + Vec2D(0, 1));
+                    replace.push((next, (next + inst, '[')));
+                    replace.push((next + Vec2D(0, 1), (next + inst + Vec2D(0, 1), ']')));
+                }
+                // Up / down large box right side. Add the left side.
+                (Some(']'), Vec2D(_, 0)) => {
+                    next_move.push(next + inst);
+                    next_move.push(next + inst + Vec2D(0, -1));
+                    replace.push((next, (next + inst, ']')));
+                    replace.push((next + Vec2D(0, -1), (next + inst + Vec2D(0, -1), '[')));
+                }
+                // Left / right or small box
+                (Some(c), _) | (Some(c), Vec2D(0, _)) | (Some(c), Vec2D(0, _))
+                    if ['O', '[', ']'].contains(c) =>
+                {
+                    next_move.push(next + inst);
+                    replace.push((next, (next + inst, *c)));
+                }
+                _ => unreachable!("Checking {next:?}"),
             }
-            next_move += inst;
         }
-        true
-    }
-
-    fn step(&mut self, inst: Vec2D<i64>, big: bool) {
-        if (!big && !self.valid_move_small(inst)) || (big && !self.valid_move_big(inst)) {
-            return;
+        for (remove, _) in &replace {
+            self.layout.remove(remove);
         }
-        let mut next_move = self.robot + inst;
+        for (_, (p, c)) in &replace {
+            self.layout.insert(*p, *c);
+        }
         self.robot += inst;
-        let mut to_insert = vec![];
-        while let Some(chr) = self.layout.remove(&next_move) {
-            next_move += inst;
-            to_insert.push((next_move, chr));
-        }
-        for (p, c) in to_insert {
-            self.layout.insert(p, c);
-        }
     }
 
     fn goods_score(&self) -> i64 {
-        self.layout.iter().fold(0, |acc, (loc, _box)| {
-            if *_box == 'O' {
+        self.layout.iter().fold(0, |acc, (loc, goods)| {
+            if ['O', '['].contains(goods) {
                 acc + (100 * loc.0) + loc.1
             } else {
                 acc
             }
         })
     }
-}
 
-impl From<&str> for Warehouse {
-    fn from(value: &str) -> Self {
+    fn new(layout: &str, big: bool) -> Self {
+        match big {
+            true => Self::new_big(layout),
+            false => Self::new_small(layout),
+        }
+    }
+
+    fn new_small(layout: &str) -> Self {
         let mut warehouse = Self::default();
-        for (point, chr) in read_grid_to_map(value) {
+        for (point, chr) in read_grid_to_map(layout) {
             match chr {
                 '#' | 'O' => {
                     warehouse
@@ -116,7 +138,37 @@ impl From<&str> for Warehouse {
         }
         warehouse
     }
+
+    fn new_big(layout: &str) -> Self {
+        let mut warehouse = Self::default();
+        for (point, chr) in read_grid_to_map(layout) {
+            match chr {
+                '#' => {
+                    warehouse
+                        .layout
+                        .insert(Vec2D(point.0 as i64, 2 * point.1 as i64), chr);
+                    warehouse
+                        .layout
+                        .insert(Vec2D(point.0 as i64, 2 * point.1 as i64 + 1), chr);
+                }
+                'O' => {
+                    warehouse
+                        .layout
+                        .insert(Vec2D(point.0 as i64, 2 * point.1 as i64), '[');
+                    warehouse
+                        .layout
+                        .insert(Vec2D(point.0 as i64, 2 * point.1 as i64 + 1), ']');
+                }
+                '@' => warehouse.robot = Vec2D(point.0 as i64, 2 * point.1 as i64),
+                _ => (),
+            }
+            warehouse.rows = warehouse.rows.max(point.0 as i64 + 1);
+            warehouse.cols = warehouse.cols.max(2 * (point.1 as i64) + 1);
+        }
+        warehouse
+    }
 }
+
 impl Display for Warehouse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in 0..self.rows {
@@ -143,50 +195,6 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_parsing() {
-        let warehouse = read_grid_to_map(
-            "########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########",
-        )
-        .iter()
-        .filter_map(|(p, c)| {
-            if ['.', '@'].contains(c) {
-                None
-            } else {
-                Some((Vec2D(p.0 as i64, p.1 as i64), *c))
-            }
-        })
-        .collect::<HashMap<_, _>>();
-        let instructions = "<^^>>>vv<v>>v<<"
-            .chars()
-            .map(|c| c.try_into().unwrap())
-            .collect::<Vec<Vec2D<i64>>>();
-        let mut day = AocDay::new(
-            "########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########
-
-<^^>>>vv
-<v>>v<<",
-        );
-        day.parse();
-        assert_eq!(warehouse, day.warehouse.layout);
-        assert_eq!(instructions, day.instructions);
-        assert_eq!(Vec2D(2, 2), day.warehouse.robot);
-    }
-
-    #[test]
     fn test_example1() {
         let mut day = AocDay::new(
             "########
@@ -204,6 +212,36 @@ mod test {
         day.parse();
         let expected = "2028";
         let actual = day.part1();
+        assert_eq!(expected, actual);
+    }
+    #[test]
+    fn test_example2() {
+        let mut day = AocDay::new(
+            "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
+        );
+        day.parse();
+        let expected = "9021";
+        let actual = day.part2();
         assert_eq!(expected, actual);
     }
 }
