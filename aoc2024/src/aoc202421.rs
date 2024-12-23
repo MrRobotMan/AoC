7bea15f5a732eb312fac9494eeb776acc94b5489
David Weiss <dweiss2@gmail.com>
2024-12-23 09:52:48 -0600

    2024 Day 21 Part 2

--- aoc2024/src/aoc202421.rs
+++ aoc2024/src/aoc202421.rs
@@ -1,34 +1,29 @@
-use std::collections::HashMap;
-
-use itertools::Itertools;
-use pathfinding::prelude::astar_bag_collect;
+use std::{collections::HashMap, sync::LazyLock};
 
 use aoc::{
     read_lines,
     runner::{output, Runner},
 };
+use itertools::Itertools;
+use pathfinding::prelude::astar_bag_collect;
 
 pub struct AocDay {
     pub(crate) input: String,
     codes: Vec<String>,
     num_pad_moves: Paths,
-    arrow_pad_moves: Paths,
 }
-
 impl Default for AocDay {
     fn default() -> Self {
         Self {
             input: String::new(),
             codes: vec![],
-            num_pad_moves: build_paths(
+            num_pad_moves: build_numpad_paths(
                 &['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A'],
                 num_pad_moves,
             ),
-            arrow_pad_moves: build_paths(&['<', 'v', '>', '^', 'A'], arrow_pad_moves),
         }
     }
 }
-
 impl AocDay {
     pub fn new<S: Into<String>>(input: S) -> Self {
         Self {
@@ -37,21 +32,14 @@ impl AocDay {
         }
     }
 
-    fn presses(&self, code: &str) -> usize {
-        let last_robot = get_presses('A', &code.chars().collect::<Vec<_>>(), &self.num_pad_moves);
-        let min = last_robot.iter().map(|p| p.len()).min().unwrap();
-        let middle_robot = last_robot
+    fn presses(&self, code: &str, robots: usize) -> usize {
+        let all_arrows =
+            get_num_presses('A', &code.chars().collect::<Vec<_>>(), &self.num_pad_moves);
+        let res = all_arrows
             .iter()
-            .filter(|p| p.len() == min)
-            .flat_map(|r| get_presses('A', r, &self.arrow_pad_moves))
+            .map(|arrows| get_arrow_presses('A', arrows, robots - 1))
             .collect::<Vec<_>>();
-        let min = middle_robot.iter().map(|p| p.len()).min().unwrap();
-        let you = middle_robot
-            .iter()
-            .filter(|p| p.len() == min)
-            .flat_map(|r| get_presses('A', r, &self.arrow_pad_moves))
-            .collect::<Vec<_>>();
-        you.iter().min_by_key(|p| p.len()).unwrap().len()
+        *res.iter().min().unwrap()
     }
 }
 
@@ -68,33 +56,92 @@ impl Runner for AocDay {
         output(
             self.codes
                 .iter()
-                .map(|c| self.presses(c) * get_numeric(c))
+                .map(|c| self.presses(c, 2) * get_numeric(c))
                 .sum::<usize>(),
         )
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        output(
+            self.codes
+                .iter()
+                .map(|c| self.presses(c, 25) * get_numeric(c))
+                .sum::<usize>(),
+        )
     }
 }
 
 fn get_numeric(code: &str) -> usize {
-    let mut v = 0;
-    for char in code.chars() {
-        match char as u8 - b'0' {
-            c if (0..=9).contains(&c) => v = v * 10 + (c as usize),
-            _ => (),
-        }
-    }
-    v
+    code[..3].parse().unwrap()
 }
+
+fn get_arrow_presses(mut cur: char, sequence: &[char], depth: usize) -> usize {
+    let mut cache = BEST_ARROW_PATHS // Key = start, end, depth. Value = steps needed.
+        .iter()
+        .map(|(k, v)| ((k.0, k.1, 0_usize), v.len()))
+        .collect::<HashMap<_, _>>();
+    let mut res = 0;
+    for &button in sequence {
+        res += inst_length(cur, button, depth, &mut cache);
+        cur = button;
+    }
+    res
+}
+
+fn inst_length(
+    start: char,
+    target: char,
+    depth: usize,
+    cache: &mut HashMap<(char, char, usize), usize>,
+) -> usize {
+    if let Some(known) = cache.get(&(start, target, depth)) {
+        return *known;
+    }
+    let mut res = 0;
+    let mut current = 'A';
+    for &button in &BEST_ARROW_PATHS[&(start, target)] {
+        res += inst_length(current, button, depth - 1, cache);
+        current = button
+    }
+    cache.insert((start, target, depth), res);
+    res
+}
+
+type Path = HashMap<(char, char), Vec<char>>;
+
+static BEST_ARROW_PATHS: LazyLock<Path> = LazyLock::new(|| {
+    HashMap::from([
+        (('A', 'A'), vec!['A']),
+        (('A', '^'), vec!['<', 'A']),
+        (('A', '>'), vec!['v', 'A']),
+        (('A', 'v'), vec!['<', 'v', 'A']),
+        (('A', '<'), vec!['v', '<', '<', 'A']),
+        (('^', 'A'), vec!['>', 'A']),
+        (('^', '^'), vec!['A']),
+        (('^', '>'), vec!['v', '>', 'A']),
+        (('^', 'v'), vec!['v', 'A']),
+        (('^', '<'), vec!['v', '<', 'A']),
+        (('v', 'A'), vec!['^', '>', 'A']),
+        (('v', '^'), vec!['^', 'A']),
+        (('v', '>'), vec!['>', 'A']),
+        (('v', 'v'), vec!['A']),
+        (('v', '<'), vec!['<', 'A']),
+        (('<', 'A'), vec!['>', '>', '^', 'A']),
+        (('<', '^'), vec!['>', '^', 'A']),
+        (('<', '>'), vec!['>', '>', 'A']),
+        (('<', 'v'), vec!['>', 'A']),
+        (('<', '<'), vec!['A']),
+        (('>', 'A'), vec!['^', 'A']),
+        (('>', '^'), vec!['<', '^', 'A']),
+        (('>', '>'), vec!['A']),
+        (('>', 'v'), vec!['<', 'A']),
+        (('>', '<'), vec!['<', '<', 'A']),
+    ])
+});
 
 type Paths = HashMap<(char, char), Vec<Vec<char>>>;
 
-fn get_presses(cur: char, buttons: &[char], paths: &Paths) -> Vec<Vec<char>> {
-    if buttons.is_empty() {
-        return vec![vec![]];
-    }
+fn get_num_presses(cur: char, buttons: &[char], paths: &Paths) -> Vec<Vec<char>> {
     if buttons.len() == 1 {
         return paths[&(cur, buttons[0])]
             .iter()
@@ -111,7 +158,7 @@ fn get_presses(cur: char, buttons: &[char], paths: &Paths) -> Vec<Vec<char>> {
         repeats += 1;
     }
     for path in &paths[&(cur, buttons[repeats])] {
-        for p in get_presses(buttons[repeats], &buttons[repeats + 1..], paths) {
+        for p in get_num_presses(buttons[repeats], &buttons[repeats + 1..], paths) {
             let mut temp = vec!['A'; repeats];
             temp.extend(path.action_list());
             temp.push('A');
@@ -122,17 +169,23 @@ fn get_presses(cur: char, buttons: &[char], paths: &Paths) -> Vec<Vec<char>> {
     res
 }
 
-fn build_paths(items: &[char], mut successors: impl FnMut(&char) -> Vec<(char, usize)>) -> Paths {
+fn build_numpad_paths(
+    items: &[char],
+    mut successors: impl FnMut(&char) -> Vec<(char, usize)>,
+) -> Paths {
     let mut res = HashMap::new();
     for pair in items.iter().permutations(2) {
         let start = pair[0];
         let end = pair[1];
-        res.insert((*start, *end), build_path(start, end, &mut successors));
+        res.insert(
+            (*start, *end),
+            build_numpad_path(start, end, &mut successors),
+        );
     }
     res
 }
 
-fn build_path(
+fn build_numpad_path(
     start: &char,
     end: &char,
     mut successors: impl FnMut(&char) -> Vec<(char, usize)>,
@@ -145,17 +198,6 @@ fn build_path(
     )
     .unwrap()
     .0
-}
-
-fn arrow_pad_moves(loc: &char) -> Vec<(char, usize)> {
-    match loc {
-        '^' => vec![('v', 1), ('A', 1)],
-        'A' => vec![('^', 1), ('>', 1)],
-        '<' => vec![('v', 1)],
-        'v' => vec![('<', 1), ('>', 1), ('^', 1)],
-        '>' => vec![('v', 1), ('A', 1)],
-        _ => unreachable!(),
-    }
 }
 
 fn num_pad_moves(loc: &char) -> Vec<(char, usize)> {
@@ -194,9 +236,6 @@ fn translate_move(value: &[char]) -> char {
         ('1', '2'),
         ('2', '3'),
         ('0', 'A'),
-        ('^', 'A'),
-        ('<', 'v'),
-        ('v', '>'),
     ];
     let lefts = [
         ('8', '7'),
@@ -206,9 +245,6 @@ fn translate_move(value: &[char]) -> char {
         ('2', '1'),
         ('3', '2'),
         ('A', '0'),
-        ('A', '^'),
-        ('v', '<'),
-        ('>', 'v'),
     ];
     let ups = [
         ('1', '4'),
@@ -219,8 +255,6 @@ fn translate_move(value: &[char]) -> char {
         ('A', '3'),
         ('3', '6'),
         ('6', '9'),
-        ('v', '^'),
-        ('>', 'A'),
     ];
     let downs = [
         ('4', '1'),
@@ -231,8 +265,6 @@ fn translate_move(value: &[char]) -> char {
         ('3', 'A'),
         ('6', '3'),
         ('9', '6'),
-        ('^', 'v'),
-        ('A', '>'),
     ];
     match (value[0], value[1]) {
         right if rights.contains(&right) => '>',
@@ -246,43 +278,16 @@ fn translate_move(value: &[char]) -> char {
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::collections::HashSet;
-
-    #[test]
-    fn test_single_path() {
-        let expected = [vec!['<', 'v', '>', 'A'], vec!['<', 'v', '^', 'A']];
-        let actual = build_path(&'<', &'A', arrow_pad_moves);
-        assert!(actual.len() == expected.len());
-        assert!(expected.contains(&actual[0]));
-        assert!(expected.contains(&actual[1]));
-    }
-
-    #[test]
-    fn test_num_paths() {
-        let expected = 9;
-        let actual = &build_paths(
-            &['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A'],
-            num_pad_moves,
-        )[&('7', 'A')];
-        assert_eq!(expected, actual.len());
-    }
 
     #[test]
     fn test_keypad_robot() {
-        let expected = HashSet::from([
-            "<A^A>^^AvvvA".to_string(),
-            "<A^A^>^AvvvA".to_string(),
-            "<A^A^^>AvvvA".to_string(),
-        ]);
-        let moves = build_paths(
+        let expected = "<A^A>^^AvvvA".chars().collect::<Vec<_>>();
+        let moves = build_numpad_paths(
             &['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A'],
             num_pad_moves,
         );
-        let actual = get_presses('A', &['0', '2', '9', 'A'], &moves)
-            .iter()
-            .map(|p| p.iter().join(""))
-            .collect::<HashSet<_>>();
-        assert_eq!(expected, actual);
+        let actual = get_num_presses('A', &['0', '2', '9', 'A'], &moves);
+        assert!(actual.contains(&expected));
     }
 
     #[test]
@@ -295,25 +300,27 @@ mod test {
     #[test]
     fn test_final_presses() {
         let day = AocDay::default();
-        let expected = 68;
-        let actual = day.presses("029A");
+        let expected = vec![68, 60, 68, 64, 64];
+        let inputs = ["029A", "980A", "179A", "456A", "379A"];
+        let actual = inputs
+            .iter()
+            .map(|code| day.presses(code, 2))
+            .collect::<Vec<_>>();
 
         assert_eq!(expected, actual);
+        assert_eq!(
+            126384,
+            actual
+                .iter()
+                .zip(inputs.iter())
+                .fold(0, |acc, (val, inp)| acc + *val * get_numeric(inp))
+        )
     }
 
     #[test]
     fn test_middle_robot() {
-        let expected = "v<<A>>^A<A>AvA<^AA>A<vAAA>^A".chars().collect::<Vec<_>>();
-        let moves = build_paths(&['<', 'v', '>', '^', 'A'], arrow_pad_moves);
-        let actual = get_presses('A', &"<A^A>^^AvvvA".chars().collect::<Vec<_>>(), &moves);
-        assert!(actual.contains(&expected));
-    }
-
-    #[test]
-    fn test_repeats() {
-        let expected = "A>A<vAAA>^A".chars().collect::<Vec<_>>();
-        let moves = build_paths(&['<', 'v', '>', '^', 'A'], arrow_pad_moves);
-        let actual = get_presses('^', &['^', 'A', 'v', 'v', 'v', 'A'], &moves);
-        assert!(actual.contains(&expected));
+        let expected = "v<<A>>^A<A>AvA^<AA>Av<AAA^>A".len();
+        let actual = get_arrow_presses('A', &"<A^A>^^AvvvA".chars().collect::<Vec<_>>(), 0);
+        assert_eq!(expected, actual);
     }
 }
