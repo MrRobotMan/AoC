fdc8300b9b5ce8fd943c4ee570c9cacbacc4e5b3
David Weiss <dweiss2@gmail.com>
2024-12-24 11:03:39 -0600

    2024 Day 24 Part 2

--- aoc2024/src/aoc202424.rs
+++ aoc2024/src/aoc202424.rs
@@ -10,7 +10,7 @@ use aoc::{
 pub struct AocDay {
     pub(crate) input: String,
     wires: HashMap<String, bool>,
-    gates: HashMap<(String, String, String), Gate>,
+    gates: Vec<(String, String, String, Gate)>, //in1, in2, out, op
     z_values: Vec<Option<bool>>,
 }
 
@@ -23,21 +23,10 @@ impl AocDay {
     }
 
     fn simulate(&mut self) -> usize {
-        while self.z_values.iter().any(|v| v.is_none()) {
-            self.process_gates();
-        }
-        let mut res = 0;
-        for val in self.z_values.iter().rev() {
-            res <<= 1;
-            if let Some(v) = val {
-                res += *v as usize;
-            }
-        }
-        res
-    }
+        let mut waiting_for_input = vec![];
 
-    fn process_gates(&mut self) {
-        for ((input1, input2, out), op) in self.gates.iter() {
+        // First pass through. Process what we can.
+        for (input1, input2, out, op) in &self.gates {
             let inp1 = self.wires.get(input1);
             let inp2 = self.wires.get(input2);
             if let (Some(in1), Some(in2)) = (inp1, inp2) {
@@ -49,8 +38,95 @@ impl AocDay {
                     .entry(out.clone())
                     .and_modify(|v| *v = value)
                     .or_insert(value);
+            } else {
+                waiting_for_input.push((input1, input2, out, op));
             }
         }
+        // Wait for input to exist, then process.
+        while !waiting_for_input.is_empty() {
+            let mut temp = vec![];
+            for (input1, input2, out, op) in waiting_for_input {
+                let inp1 = self.wires.get(input1);
+                let inp2 = self.wires.get(input2);
+                if let (Some(in1), Some(in2)) = (inp1, inp2) {
+                    let value = op.process(*in1, *in2);
+                    if let Some(idx) = out.strip_prefix('z') {
+                        self.z_values[idx.parse::<usize>().unwrap()] = Some(value);
+                    }
+                    self.wires
+                        .entry(out.clone())
+                        .and_modify(|v| *v = value)
+                        .or_insert(value);
+                } else {
+                    temp.push((input1, input2, out, op));
+                }
+            }
+            waiting_for_input = temp;
+        }
+        value(&self.z_values.iter().filter_map(|v| *v).collect::<Vec<_>>())
+    }
+
+    fn find_swapped_gates(&self) -> Vec<String> {
+        // Half adder (x0, y0) and full adders chained send output to z.
+        // Adder output => z, Adder Carry to c except last carry should go to z.
+        //
+        // Half adder
+        // x00 XOR y00 => z00
+        // x00 AND y00 => c00
+        //
+        // Full adder
+        // xn XOR yn => tempn
+        // tempn XOR cn-1 => zn
+        // xn AND yn => temp2n
+        // tempn AND cn-1 => temp3n
+        // temp3n OR temp2n => cn
+
+        let mut mismatched = vec![];
+        // Find how wires chain together.
+        let mut wire_map = HashMap::new();
+        for (in1, in2, out, op) in &self.gates {
+            wire_map.entry(in1).or_insert(vec![]).push((op, out));
+            wire_map.entry(in2).or_insert(vec![]).push((op, out));
+        }
+        for (in1, in2, out, op) in &self.gates {
+            let chain = wire_map.get(out);
+            let contains_xor = chain_contains_op(&chain, Gate::Xor);
+            let contains_or = chain_contains_op(&chain, Gate::Or);
+            let contains_and = chain_contains_op(&chain, Gate::And);
+            // Handle initial half adder
+            let is_half_adder = in1.ends_with("00") && in2.ends_with("00");
+
+            let is_input_bit = (in1.starts_with('x') && in2.starts_with('y'))
+                || (in1.starts_with('y') && in2.starts_with('x'));
+            let is_result_bit = out.starts_with('z');
+            let last_bit = *out == format!("z{}", self.z_values.len() - 1);
+            let is_valid = match op {
+                Gate::And => {
+                    // Half adder carry bit
+                    // Or feeds into a carry bit.
+                    is_half_adder || contains_or
+                }
+                Gate::Or => {
+                    // Carries from full adders.
+                    // Outputs last bit
+                    // Carry bit is (prev carry AND inp1 XOR inp2) OR (inp1 AND inp2)
+                    last_bit || (contains_and && contains_xor)
+                }
+                Gate::Xor => {
+                    // Half adder should output the least significant bit.
+                    // If a full adder outputs a bit the inputs should be from other ops.
+                    // x and y inputs must be xor'd again.
+                    (is_half_adder && out == "z00")
+                        || (!is_input_bit && is_result_bit)
+                        || (is_input_bit && contains_xor)
+                }
+            };
+            if !is_valid {
+                mismatched.push(out.clone());
+            }
+        }
+        mismatched.sort();
+        mismatched
     }
 }
 
@@ -64,17 +140,20 @@ impl Runner for AocDay {
         for line in lines {
             match line.split_once(": ") {
                 Some((wire, value)) => {
-                    self.wires.insert(wire.into(), matches!(value, "1"));
+                    let value = matches!(value, "1");
+                    self.wires.insert(wire.into(), value);
                 }
                 None => {
                     let parts = line.split_whitespace().collect::<Vec<_>>();
                     if parts[4].starts_with("z") {
                         self.z_values.push(None);
                     }
-                    self.gates.insert(
-                        (parts[0].into(), parts[2].into(), parts[4].into()),
+                    self.gates.push((
+                        parts[0].into(),
+                        parts[2].into(),
+                        parts[4].into(),
                         parts[1].into(),
-                    );
+                    ));
                 }
             }
         }
@@ -85,11 +164,23 @@ impl Runner for AocDay {
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        output(self.find_swapped_gates().join(","))
     }
 }
 
-#[derive(Debug)]
+fn chain_contains_op(chain: &Option<&Vec<(&Gate, &String)>>, gate: Gate) -> bool {
+    chain.is_some_and(|v| v.iter().any(|(&g, _)| g == gate))
+}
+
+fn value(wires: &[bool]) -> usize {
+    let mut res = 0;
+    for (idx, wire) in wires.iter().enumerate() {
+        res |= (*wire as usize) << idx
+    }
+    res
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Gate {
     And,
     Or,
@@ -175,6 +266,13 @@ tnw OR pbm -> gnj",
         day.parse();
         let expected = 2024;
         let actual = day.simulate();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_create_number() {
+        let expected = 4;
+        let actual = value(&[false, false, true]);
         assert_eq!(expected, actual);
     }
 }
