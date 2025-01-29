1e29f962ea6639aff11480e375e31f03a2c4428b
David Weiss <dweiss2@gmail.com>
2025-01-28 21:54:05 -0600

    2021 Day 14 Part 2

--- aoc2021/src/aoc202114.rs
+++ aoc2021/src/aoc202114.rs
@@ -30,12 +30,15 @@ impl Runner for AocDay {
     }
 
     fn part1(&mut self) -> String {
-        let poly = (0..10).fold(self.polymer.clone(), |acc, _| acc.step());
-        output(poly.most().1 - poly.least().1)
+        let mut poly = self.polymer.clone();
+        poly.step(10);
+        output(poly.delta())
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        let mut poly = self.polymer.clone();
+        poly.step(40);
+        output(poly.delta())
     }
 }
 
@@ -44,31 +47,75 @@ struct Polymer {
     template: Vec<char>,
     rules: HashMap<(char, char), char>,
     count: HashMap<char, usize>,
+    pairs: HashMap<(char, char), usize>,
 }
 
 impl Polymer {
-    fn step(&self) -> Self {
-        let mut poly = Self {
-            template: vec![self.template[0]],
-            rules: self.rules.clone(),
-            ..Default::default()
-        };
+    fn step(&mut self, count: usize) {
+        for _ in 0..count {
+            let mut pairs = HashMap::new();
+            for ((left, right), cnt) in self.pairs.iter() {
+                let insert = self.rules[&(*left, *right)];
+                let v = pairs.entry((*left, insert)).or_default();
+                *v += cnt;
+                let v = pairs.entry((insert, *right)).or_default();
+                *v += cnt;
+            }
+            self.pairs = pairs;
+        }
+    }
+
+    fn init(&mut self) {
+        self.count();
         for pair in self.template.windows(2) {
-            poly.template.push(self.rules[&(pair[0], pair[1])]);
-            poly.template.push(pair[1]);
+            self.pairs
+                .entry((pair[0], pair[1]))
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
         }
-        for chr in &poly.template {
-            poly.count.entry(*chr).and_modify(|v| *v += 1).or_insert(1);
-        }
-        poly
     }
 
-    fn most(&self) -> (&char, &usize) {
-        self.count.iter().max_by(|a, b| a.1.cmp(b.1)).unwrap()
+    fn count(&mut self) {
+        let mut new_count = HashMap::new();
+        for ((left, right), count) in &self.pairs {
+            let l = new_count.entry(*left).or_default();
+            *l += count;
+
+            let r = new_count.entry(*right).or_default();
+            *r += count;
+        }
+        // Counts everything twice except the first and last characters due to counting in pairs.
+        new_count.entry(self.template[0]).and_modify(|v| *v += 1);
+        new_count
+            .entry(self.template[self.template.len() - 1])
+            .and_modify(|v| *v += 1);
+        for v in new_count.values_mut() {
+            *v /= 2;
+        }
+        self.count = new_count;
     }
 
-    fn least(&self) -> (&char, &usize) {
-        self.count.iter().min_by(|a, b| a.1.cmp(b.1)).unwrap()
+    fn most(&self) -> usize {
+        *self
+            .count
+            .iter()
+            .max_by(|a, b| a.1.cmp(b.1))
+            .unwrap_or((&'_', &1))
+            .1
+    }
+
+    fn least(&self) -> usize {
+        *self
+            .count
+            .iter()
+            .min_by(|a, b| a.1.cmp(b.1))
+            .unwrap_or((&'_', &0))
+            .1
+    }
+
+    fn delta(&mut self) -> usize {
+        self.count();
+        self.most() - self.least()
     }
 }
 
@@ -83,6 +130,7 @@ impl From<Vec<String>> for Polymer {
                 .rules
                 .insert((chars[0], chars[1]), chars[chars.len() - 1]);
         }
+        polymer.init();
         polymer
     }
 }
@@ -115,19 +163,10 @@ mod test {
             ]),
             ..Default::default()
         };
-        let expected = &"NCNBCHB".chars().collect::<Vec<_>>();
-        poly = poly.step();
-        let actual = &poly.template;
-        assert_eq!(expected, actual);
-        for _ in 0..3 {
-            poly = poly.step();
-        }
-        let expected = &"NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-            .chars()
-            .collect::<Vec<_>>();
-        let actual = &poly.template;
-        assert_eq!(expected, actual);
-        poly = (0..6).fold(poly, |acc, _| acc.step());
-        assert_eq!(poly.most().1 - poly.least().1, 1588);
+        poly.init();
+        poly.step(10);
+        let delta = poly.delta();
+        println!("\nCount\n{:?}", poly.count);
+        assert_eq!(delta, 1588);
     }
 }
