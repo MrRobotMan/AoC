786d3f561381d4545a83fdc272a3fb08ea64acd0
David Weiss <dweiss2@gmail.com>
2025-02-02 20:10:06 -0600

    2021 Day 16 Part 2

--- aoc2021/src/aoc202116.rs
+++ aoc2021/src/aoc202116.rs
@@ -33,52 +33,49 @@ impl Runner for AocDay {
     }
 
     fn part2(&mut self) -> String {
-        output("Unsolved")
+        output(self.packet.eval())
     }
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
 struct Packet {
     version: usize,
-    type_id_num: usize,
     type_id: TypeID,
+    op_type: OpType,
     sub_packets: Vec<Packet>,
-    length: usize,
 }
 
 impl Packet {
     fn new(binary: &[char]) -> Self {
         let version = from_binary(&binary[..3]);
-        let type_id_num = from_binary(&binary[3..6]);
         let type_id = binary[3..].into();
-        let mut length;
+        let op_type = binary[3..].into();
         let mut sub_packets = vec![];
-        match type_id {
-            TypeID::Literal((_, len)) => length = len,
-            TypeID::Length(mut l) => {
-                length = 22; // 3 version, 3 type, 1 operator, 15 length
+        match op_type {
+            OpType::Literal(_) => (),
+            OpType::Length(mut l) => {
+                let mut start = 22; // 3 version, 3 type, 1 operator, 15 length
                 while l > 0 {
-                    let packet = Packet::new(&binary[length..]);
-                    l -= packet.length;
-                    length += packet.length;
+                    let packet = Packet::new(&binary[start..]);
+                    l -= packet.length();
+                    start += packet.length();
                     sub_packets.push(packet);
                 }
             }
-            TypeID::Count(c) => {
-                length = 18; // 3 version, 3 type, 1 operator, 11 count
+            OpType::Count(c) => {
+                let mut start = 18; // 3 version, 3 type, 1 operator, 11 count
                 for _ in 0..c {
-                    let packet = Packet::new(&binary[length..]);
-                    length += packet.length;
+                    let packet = Packet::new(&binary[start..]);
+                    start += packet.length();
                     sub_packets.push(packet);
                 }
             }
         }
         Self {
             version,
-            type_id_num,
             type_id,
+            op_type,
             sub_packets,
-            length,
         }
     }
 
@@ -89,6 +86,27 @@ impl Packet {
                 .iter()
                 .map(Packet::version_sum)
                 .sum::<usize>()
+    }
+
+    fn length(&self) -> usize {
+        match self.op_type {
+            OpType::Literal(l) => l,
+            OpType::Length(_) => self.sub_packets.iter().fold(22, |acc, p| acc + p.length()),
+            OpType::Count(_) => self.sub_packets.iter().fold(18, |acc, p| acc + p.length()),
+        }
+    }
+
+    fn eval(&self) -> usize {
+        match self.type_id {
+            TypeID::Literal(v) => v,
+            TypeID::Sum => self.sub_packets.iter().fold(0, |acc, p| acc + p.eval()),
+            TypeID::Product => self.sub_packets.iter().fold(1, |acc, p| acc * p.eval()),
+            TypeID::Min => self.sub_packets.iter().map(Packet::eval).min().unwrap(),
+            TypeID::Max => self.sub_packets.iter().map(Packet::eval).max().unwrap(),
+            TypeID::Greater => (self.sub_packets[0].eval() > self.sub_packets[1].eval()) as usize,
+            TypeID::Less => (self.sub_packets[0].eval() < self.sub_packets[1].eval()) as usize,
+            TypeID::Equal => (self.sub_packets[0].eval() == self.sub_packets[1].eval()) as usize,
+        }
     }
 }
 
@@ -103,23 +121,52 @@ fn parse_literal_to_end(value: &[char]) -> (usize, usize) {
     unreachable!("Never reached end of literal")
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
 enum TypeID {
-    Literal((usize, usize)),
-    Length(usize),
-    Count(usize),
-}
-
-impl Default for TypeID {
-    fn default() -> Self {
-        Self::Literal((0, 0))
-    }
+    Literal(usize),
+    #[default]
+    Sum,
+    Product,
+    Min,
+    Max,
+    Greater,
+    Less,
+    Equal,
 }
 
 impl From<&[char]> for TypeID {
     fn from(value: &[char]) -> Self {
+        match from_binary(&value[..3]) {
+            4 => Self::Literal(parse_literal_to_end(&value[3..]).0),
+            0 => Self::Sum,
+            1 => Self::Product,
+            2 => Self::Min,
+            3 => Self::Max,
+            5 => Self::Greater,
+            6 => Self::Less,
+            7 => Self::Equal,
+            _ => unreachable!("Bad Type ID {value:?}"),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum OpType {
+    Literal(usize),
+    Length(usize),
+    Count(usize),
+}
+
+impl Default for OpType {
+    fn default() -> Self {
+        Self::Literal(0)
+    }
+}
+
+impl From<&[char]> for OpType {
+    fn from(value: &[char]) -> Self {
         match (from_binary(&value[..3]), value[3]) {
-            (4, _) => Self::Literal(parse_literal_to_end(&value[3..])),
+            (4, _) => Self::Literal(parse_literal_to_end(&value[3..]).1),
             (_, '0') => Self::Length(from_binary(&value[4..19])),
             (_, '1') => Self::Count(from_binary(&value[4..15])),
             _ => unreachable!("Bad Type ID {value:?}"),
@@ -167,10 +214,9 @@ mod test {
     fn test_basic_parse() {
         let expected = Packet {
             version: 6,
-            type_id_num: 4,
-            type_id: TypeID::Literal((2021, 21)),
+            type_id: TypeID::Literal(2021),
+            op_type: OpType::Literal(21),
             sub_packets: vec![],
-            length: 21,
         };
         let actual = Packet::new(&from_hex("D2FE28").chars().collect::<Vec<_>>());
         assert_eq!(expected, actual);
@@ -180,25 +226,22 @@ mod test {
     fn test_nested() {
         let expected = Packet {
             version: 1,
-            type_id_num: 6,
-            type_id: TypeID::Length(27),
+            type_id: TypeID::Less,
+            op_type: OpType::Length(27),
             sub_packets: vec![
                 Packet {
                     version: 6,
-                    type_id_num: 4,
-                    type_id: TypeID::Literal((10, 11)),
+                    type_id: TypeID::Literal(10),
+                    op_type: OpType::Literal(11),
                     sub_packets: vec![],
-                    length: 11,
                 },
                 Packet {
                     version: 2,
-                    type_id_num: 4,
-                    type_id: TypeID::Literal((20, 16)),
+                    type_id: TypeID::Literal(20),
+                    op_type: OpType::Literal(16),
                     sub_packets: vec![],
-                    length: 16,
                 },
             ],
-            length: 49,
         };
         let actual = Packet::new(&from_hex("38006F45291200").chars().collect::<Vec<_>>());
         assert_eq!(expected, actual);
@@ -208,32 +251,28 @@ mod test {
     fn test_nested_triple() {
         let expected = Packet {
             version: 7,
-            type_id_num: 3,
-            type_id: TypeID::Count(3),
+            type_id: TypeID::Max,
+            op_type: OpType::Count(3),
             sub_packets: vec![
                 Packet {
                     version: 2,
-                    type_id_num: 4,
-                    type_id: TypeID::Literal((1, 11)),
+                    type_id: TypeID::Literal(1),
+                    op_type: OpType::Literal(11),
                     sub_packets: vec![],
-                    length: 11,
                 },
                 Packet {
                     version: 4,
-                    type_id_num: 4,
-                    type_id: TypeID::Literal((2, 11)),
+                    type_id: TypeID::Literal(2),
+                    op_type: OpType::Literal(11),
                     sub_packets: vec![],
-                    length: 11,
                 },
                 Packet {
                     version: 1,
-                    type_id_num: 4,
-                    type_id: TypeID::Literal((3, 11)),
+                    type_id: TypeID::Literal(3),
+                    op_type: OpType::Literal(11),
                     sub_packets: vec![],
-                    length: 11,
                 },
             ],
-            length: 51,
         };
         let actual = Packet::new(&from_hex("EE00D40C823060").chars().collect::<Vec<_>>());
         assert_eq!(expected, actual);
