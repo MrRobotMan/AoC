551a7062163b3897467765d87a3498caea9e5dee
David Weiss <ebdbbb@proton.me>
2025-12-01 20:33:10 -0600

    2025 Day 1 Part 2

--- aoc2025/src/bin/aoc202501.rs
+++ aoc2025/src/bin/aoc202501.rs
@@ -2,6 +2,9 @@ use std::str::FromStr;
 
 use puzlib::read_lines;
 
+const START: i32 = 50;
+const SIZE: i32 = 100;
+
 fn main() {
     println!("---- 2025: 01 ----");
     let input = read_lines("aoc2025/inputs/day01.txt");
@@ -16,7 +19,7 @@ fn parse(input: &[String]) -> Vec<Direction> {
 }
 
 fn part1(model: &[Direction]) -> usize {
-    let mut pos = 50;
+    let mut pos = START;
     model
         .iter()
         .filter_map(|dir| {
@@ -26,18 +29,37 @@ fn part1(model: &[Direction]) -> usize {
         .count()
 }
 
-fn part2(_model: &[Direction]) -> String {
-    "Unsolved".into()
+fn part2(model: &[Direction]) -> i32 {
+    let mut pos = 50;
+    let mut count = 0;
+    for dir in model.iter() {
+        match dir {
+            Direction::Right(n) => {
+                count += n / SIZE;
+                if pos > 0 && pos + (n % SIZE) >= SIZE {
+                    count += 1;
+                }
+            }
+            Direction::Left(n) => {
+                count += n / SIZE;
+                if pos > 0 && pos - (n % SIZE) <= 0 {
+                    count += 1;
+                }
+            }
+        }
+        pos = rotate(pos, *dir);
+    }
+    count
 }
 
 fn rotate(start: i32, dir: Direction) -> i32 {
     match dir {
-        Direction::Right(n) => (start + n) % 100,
-        Direction::Left(n) => (start - n).rem_euclid(100),
+        Direction::Right(n) => (start + n) % SIZE,
+        Direction::Left(n) => (start - n).rem_euclid(SIZE),
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 enum Direction {
     Right(i32),
     Left(i32),
@@ -72,6 +94,27 @@ mod test {
     fn test_example2() {
         let expected = 98;
         let actual = rotate(1, Direction::Left(3));
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_spins() {
+        use Direction::*;
+        let expected = 9;
+        let model = [
+            Left(68),
+            Left(30),
+            Right(48),
+            Left(5),
+            Right(60),
+            Left(55),
+            Left(1),
+            Left(99),
+            Right(14),
+            Left(82),
+            Left(240),
+        ];
+        let actual = part2(&model);
         assert_eq!(expected, actual);
     }
 }
