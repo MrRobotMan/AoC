6c9eab183eeb2a659a55774bfd983a79a6757077
David Weiss <ebdbbb@proton.me>
2025-12-03 18:00:20 -0600

    Added 2025 Day 3

---
+++ aoc2025/src/bin/aoc202503.rs
@@ -0,0 +1,35 @@
+use puzlib;
+fn main() {
+    println!("---- 2025: 03 ----");
+    let input = puzlib::read_lines("aoc2025/inputs/day03.txt");
+    println!("Parsing");
+    let model = parse(input);
+    println!("Part 1: {}", part1(&model));
+    println!("Part 2: {}", part2(&model));
+}
+
+fn parse(input: &str) -> String {
+    input.into()
+}
+
+fn part1(_model: &str) -> String {
+    "Unsolved".into()
+}
+
+fn part2(_model: &str) -> String {
+    "Unsolved".into()
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        let expected = 0;
+        let actual = 0;
+        assert_eq!(expected, actual);
+    }
+}
+        
