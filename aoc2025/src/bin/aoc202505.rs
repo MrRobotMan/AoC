32b6e681ac423a28a40fe71b97371a48ede2c7bc
David Weiss <ebdbbb@proton.me>
2025-12-05 13:28:33 -0600

    2025 Day 5 Part 2

--- aoc2025/src/bin/aoc202505.rs
+++ aoc2025/src/bin/aoc202505.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Parsing");
     let (ranges, ids) = parse(input);
     println!("Part 1: {}", part1(&ranges, &ids));
-    println!("Part 2: {}", part2(&ranges, &ids));
+    println!("Part 2: {}", part2(&ranges));
 }
 
 fn parse(input: Vec<String>) -> (Vec<(usize, usize)>, Vec<usize>) {
@@ -25,8 +25,11 @@ fn part1(ranges: &[(usize, usize)], ids: &[usize]) -> usize {
     ids.iter().filter(|id| in_range(ranges, **id)).count()
 }
 
-fn part2(_ranges: &[(usize, usize)], _ids: &[usize]) -> String {
-    "Unsolved".into()
+fn part2(ranges: &[(usize, usize)]) -> usize {
+    let ranges = merge_ranges(ranges);
+    ranges
+        .iter()
+        .fold(0, |acc, (start, end)| acc + (end - start + 1))
 }
 
 fn in_range(ranges: &[(usize, usize)], id: usize) -> bool {
@@ -36,4 +39,46 @@ fn in_range(ranges: &[(usize, usize)], id: usize) -> bool {
         }
     }
     false
+}
+
+fn merge_ranges(ranges: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    let mut merged = vec![];
+    let (mut start, mut end) = ranges[0];
+    for range in ranges.iter().skip(1) {
+        if range.0 <= end {
+            end = end.max(range.1);
+        } else {
+            merged.push((start, end));
+            (start, end) = *range;
+        }
+    }
+    merged.push((start, end));
+    merged
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_merged() {
+        let expected = vec![(1, 6), (10, 12), (21, 40)];
+        let actual = merge_ranges(&[
+            (1, 3),
+            (2, 5),
+            (5, 6),
+            (10, 12),
+            (21, 30),
+            (25, 40),
+            (30, 32),
+        ]);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_merged2() {
+        let expected = vec![(3, 5), (10, 14), (16, 20)];
+        let actual = merge_ranges(&[(3, 5), (10, 14), (11, 13), (16, 20), (12, 18)]);
+        assert_eq!(expected, actual);
+    }
 }
