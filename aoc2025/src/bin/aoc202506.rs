b8c7f2c0bb9457275e1bbeb58116a6cbed93ce10
David Weiss <ebdbbb@proton.me>
2025-12-06 09:11:57 -0600

    2025 Day 6 Part 1

--- aoc2025/src/bin/aoc202506.rs
+++ aoc2025/src/bin/aoc202506.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 
 fn main() {
     println!("---- 2025: 06 ----");
@@ -8,28 +9,62 @@ fn main() {
     println!("Part 2: {}", part2(&model));
 }
 
-fn parse(input: Vec<String>) -> String {
-    "unparsed".into()
+fn parse(input: Vec<String>) -> Vec<Operation> {
+    let mut ops = input[0]
+        .split_whitespace()
+        .map(|value| Operation {
+            numbers: vec![value.parse().unwrap()],
+            operator: Operator::None,
+        })
+        .collect::<Vec<_>>();
+    for line in input.into_iter().skip(1) {
+        for (idx, val) in line.split_whitespace().enumerate() {
+            match val.parse::<i64>() {
+                Ok(n) => ops[idx].numbers.push(n),
+                Err(_) => ops[idx].operator = val.parse().unwrap(),
+            }
+        }
+    }
+    ops
 }
 
-fn part1(_model: &str) -> String {
+fn part1(model: &[Operation]) -> i64 {
+    model.iter().map(Operation::process).sum()
+}
+
+fn part2(_model: &[Operation]) -> String {
     "Unsolved".into()
 }
 
-fn part2(_model: &str) -> String {
-    "Unsolved".into()
+struct Operation {
+    numbers: Vec<i64>,
+    operator: Operator,
 }
 
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_example1() {
-        let expected = 0;
-        let actual = 0;
-        assert_eq!(expected, actual);
+impl Operation {
+    fn process(&self) -> i64 {
+        match self.operator {
+            Operator::Add => self.numbers.iter().fold(0, |acc, n| acc + *n),
+            Operator::Mul => self.numbers.iter().fold(1, |acc, n| acc * *n),
+            Operator::None => panic!("None operator found."),
+        }
     }
 }
-        
+
+enum Operator {
+    Add,
+    Mul,
+    None,
+}
+
+impl FromStr for Operator {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "+" => Ok(Self::Add),
+            "*" => Ok(Self::Mul),
+            _ => Err(format!("Unknown operator {s}")),
+        }
+    }
+}
