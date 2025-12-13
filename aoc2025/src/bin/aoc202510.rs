8c9ae81dfbd804ddb2f9bc40561b782d82a5628e
David Weiss <ebdbbb@proton.me>
2025-12-13 16:26:33 -0600

    2025 Day 10 Part 2 with help from Reddit (u/tenthmascot)

--- aoc2025/src/bin/aoc202510.rs
+++ aoc2025/src/bin/aoc202510.rs
@@ -1,7 +1,9 @@
 use std::{
-    collections::{BinaryHeap, HashMap, VecDeque},
+    collections::{HashMap, VecDeque},
     str::FromStr,
 };
+
+use puzlib::Combinations;
 
 fn main() {
     println!("---- 2025: 10 ----");
@@ -63,37 +65,37 @@ impl Machine {
     }
 
     fn configure_joltage(&self) -> usize {
-        let mut state = HashMap::new();
-        let mut queue = BinaryHeap::new();
-        let max_idx = self.jolts.len() - 1;
-        queue.push(State::new(&vec![0_usize; self.jolts.len()], &[]));
-        while let Some(State {
-            joltage: cur,
-            buttons: list,
-        }) = queue.pop()
-        {
-            for button in &self.buttons {
-                let mut next = list.clone();
-                next.push(*button);
-                let value = cur
-                    .iter()
-                    .enumerate()
-                    .map(|(idx, val)| val + (1 << (max_idx - idx) & button).count_ones() as usize)
+        let joltage_parity_patterns = self.parity_patterns();
+        find_best_joltage(
+            self.jolts.clone(),
+            &joltage_parity_patterns,
+            &mut HashMap::new(),
+        )
+    }
+
+    fn parity_patterns(&self) -> HashMap<usize, HashMap<Vec<usize>, usize>> {
+        let mut joltage_parity_patterns =
+            HashMap::from_iter((0..(1 << self.jolts.len())).map(|p| (p, HashMap::new())));
+        for buttons_pressed in 0..self.buttons.len() + 1 {
+            for button_combination in self.buttons.choose(buttons_pressed) {
+                let pattern = (0..self.jolts.len())
+                    .rev()
+                    .map(|idx| {
+                        button_combination.iter().fold(0, |acc, button| {
+                            acc + (button & 1 << idx).count_ones() as usize
+                        })
+                    })
                     .collect::<Vec<_>>();
-                if value.iter().enumerate().any(|(i, v)| v > &self.jolts[i]) {
-                    continue;
-                }
-                if value == self.jolts {
-                    return next.len();
-                }
-                let s = state.entry(value.clone()).or_insert(next.clone());
-                if s.len() > next.len() || *s == next {
-                    *s = next.clone();
-                    queue.push(State::new(&value, &next));
-                }
+                let parity_pattern = pattern.iter().fold(0, |acc, v| (acc << 1) + (v % 2));
+                joltage_parity_patterns
+                    .entry(parity_pattern)
+                    .and_modify(|e| {
+                        e.entry(pattern).or_insert(buttons_pressed);
+                    });
             }
         }
-        panic!("Can't get joltage.")
+
+        joltage_parity_patterns
     }
 }
 
@@ -129,40 +131,35 @@ impl FromStr for Machine {
         })
     }
 }
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct State {
-    joltage: Vec<usize>,
-    buttons: Vec<usize>,
-}
-
-impl State {
-    fn new(joltage: &[usize], buttons: &[usize]) -> Self {
-        Self {
-            joltage: joltage.to_vec(),
-            buttons: buttons.to_vec(),
-        }
+fn find_best_joltage(
+    target: Vec<usize>,
+    patterns: &HashMap<usize, HashMap<Vec<usize>, usize>>,
+    cache: &mut HashMap<Vec<usize>, usize>,
+) -> usize {
+    // Credit to u/tentchmascot for this method.
+    // https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+    if let Some(result) = cache.get(&target) {
+        return *result;
     }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+    if target.iter().all(|v| v == &0) {
+        return 0;
     }
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Min heap
-        match other.buttons.len().cmp(&self.buttons.len()) {
-            std::cmp::Ordering::Equal => self
-                .joltage
+    let mut best = usize::MAX;
+    let pp = target.iter().fold(0, |acc, v| (acc << 1) + (v % 2));
+    for (pattern, cost) in &patterns[&pp] {
+        if pattern.iter().zip(target.iter()).all(|(p, t)| p <= t) {
+            let next_target = pattern
                 .iter()
-                .sum::<usize>()
-                .cmp(&other.joltage.iter().sum::<usize>()),
-            v => v,
+                .zip(target.iter())
+                .map(|(p, t)| (t - p) / 2)
+                .collect::<Vec<_>>();
+            let next_res = find_best_joltage(next_target, patterns, cache);
+            if next_res < usize::MAX / 2 {
+                best = best.min(cost + 2 * next_res);
+            }
         }
     }
+    best
 }
 
 #[cfg(test)]
@@ -182,5 +179,141 @@ mod test {
         assert_eq!(expected, actual);
         assert_eq!(2, actual.configure_lights());
         assert_eq!(10, actual.configure_joltage());
+    }
+
+    #[test]
+    fn test_patterns() {
+        let machine = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+            .parse::<Machine>()
+            .unwrap();
+        let expected = HashMap::from([
+            (
+                0,
+                HashMap::from([
+                    (vec![0, 0, 0, 0], 0),
+                    (vec![0, 0, 2, 2], 3),
+                    (vec![2, 2, 2, 2], 4),
+                ]),
+            ),
+            (
+                1,
+                HashMap::from([
+                    (vec![0, 0, 0, 1], 1),
+                    (vec![0, 0, 2, 1], 2),
+                    (vec![2, 2, 2, 1], 4),
+                    (vec![2, 2, 2, 3], 5),
+                ]),
+            ),
+            (
+                2,
+                HashMap::from([
+                    (vec![0, 0, 1, 0], 1),
+                    (vec![0, 0, 1, 2], 2),
+                    (vec![2, 2, 1, 2], 4),
+                    (vec![2, 2, 3, 2], 5),
+                ]),
+            ),
+            (
+                3,
+                HashMap::from([
+                    (vec![0, 0, 1, 1], 1),
+                    (vec![2, 2, 1, 1], 3),
+                    (vec![2, 2, 3, 3], 6),
+                ]),
+            ),
+            (
+                4,
+                HashMap::from([
+                    (vec![0, 1, 0, 2], 2),
+                    (vec![0, 1, 2, 2], 3),
+                    (vec![2, 1, 2, 0], 3),
+                    (vec![2, 1, 2, 2], 4),
+                ]),
+            ),
+            (
+                5,
+                HashMap::from([
+                    (vec![0, 1, 0, 1], 1),
+                    (vec![2, 1, 2, 1], 3),
+                    (vec![0, 1, 2, 3], 4),
+                ]),
+            ),
+            (
+                6,
+                HashMap::from([
+                    (vec![0, 1, 1, 2], 2),
+                    (vec![2, 1, 1, 0], 2),
+                    (vec![2, 1, 3, 2], 5),
+                ]),
+            ),
+            (
+                7,
+                HashMap::from([
+                    (vec![0, 1, 1, 1], 2),
+                    (vec![0, 1, 1, 3], 3),
+                    (vec![2, 1, 1, 1], 3),
+                    (vec![2, 1, 3, 1], 4),
+                ]),
+            ),
+            (
+                8,
+                HashMap::from([
+                    (vec![1, 0, 2, 0], 2),
+                    (vec![1, 2, 0, 2], 3),
+                    (vec![1, 0, 2, 2], 3),
+                    (vec![1, 2, 2, 2], 4),
+                ]),
+            ),
+            (
+                9,
+                HashMap::from([
+                    (vec![1, 2, 0, 1], 2),
+                    (vec![1, 0, 2, 1], 2),
+                    (vec![1, 2, 2, 3], 5),
+                ]),
+            ),
+            (
+                10,
+                HashMap::from([
+                    (vec![1, 0, 1, 0], 1),
+                    (vec![1, 2, 1, 2], 3),
+                    (vec![1, 0, 3, 2], 4),
+                ]),
+            ),
+            (
+                11,
+                HashMap::from([
+                    (vec![1, 0, 1, 1], 2),
+                    (vec![1, 2, 1, 1], 3),
+                    (vec![1, 0, 3, 1], 3),
+                    (vec![1, 2, 1, 3], 4),
+                ]),
+            ),
+            (
+                12,
+                HashMap::from([(vec![1, 1, 0, 0], 1), (vec![1, 1, 2, 2], 3)]),
+            ),
+            (
+                13,
+                HashMap::from([
+                    (vec![1, 1, 0, 1], 2),
+                    (vec![1, 1, 2, 1], 3),
+                    (vec![1, 1, 2, 3], 4),
+                ]),
+            ),
+            (
+                14,
+                HashMap::from([
+                    (vec![1, 1, 1, 0], 2),
+                    (vec![1, 1, 1, 2], 3),
+                    (vec![1, 1, 3, 2], 4),
+                ]),
+            ),
+            (
+                15,
+                HashMap::from([(vec![1, 1, 1, 1], 2), (vec![1, 1, 3, 3], 5)]),
+            ),
+        ]);
+        assert_eq!(machine.parity_patterns(), expected);
     }
 }
